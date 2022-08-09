add0f61c3fce91fedbec327b636b5e5a3b8d293c
James Knighton <iamknighton@gmail.com>
2022-08-09 13:32:29 -0700

    Do the others.

---
+++ streaming/base/format/json/README.md
@@ -0,0 +1,50 @@
+Example:
+
+    {
+      "columns": {
+        "number": "int",
+        "words": "str"
+      },
+      "compression": "zstd:7",
+      "format": "json",
+      "hashes": [
+        "sha1",
+        "xxh3_64"
+      ],
+      "newline": "\n",
+      "raw_data": {
+        "basename": "shard.00000.json",
+        "bytes": 1048546,
+        "hashes": {
+          "sha1": "bfb6509ba6f041726943ce529b36a1cb74e33957",
+          "xxh3_64": "0eb102a981b299eb"
+        }
+      },
+      "raw_meta": {
+        "basename": "shard.00000.json.meta",
+        "bytes": 53590,
+        "hashes": {
+          "sha1": "15ae80e002fe625b0b18f1a45058532ee867fa9b",
+          "xxh3_64": "7b113f574a422ac1"
+        }
+      },
+      "samples": 13352,
+      "size_limit": 1048576,
+      "version": 2,
+      "zip_data": {
+        "basename": "shard.00000.json.zstd",
+        "bytes": 149268,
+        "hashes": {
+          "sha1": "7d45c600a71066ca8d43dbbaa2ffce50a91b735e",
+          "xxh3_64": "3d338d4826d4b5ac"
+        }
+      },
+      "zip_meta": {
+        "basename": "shard.00000.json.meta.zstd",
+        "bytes": 42180,
+        "hashes": {
+          "sha1": "f64477cca5d27fc3a0301eeb4452ef7310cbf670",
+          "xxh3_64": "6e2b364f4e78670d"
+        }
+      }
+    }
--- streaming/base/format/xsv/reader.py
+++ streaming/base/format/xsv/reader.py
@@ -8,63 +8,6 @@ from typing_extensions import Self
 
 from ..base.reader import FileInfo, SplitReader
 from .encodings import xsv_decode
-'''
-Example:
-
-    {
-      "column_encodings": [
-        "int",
-        "str"
-      ],
-      "column_names": [
-        "number",
-        "words"
-      ],
-      "compression": "zstd:7",
-      "format": "csv",
-      "hashes": [
-        "sha1",
-        "xxh3_64"
-      ],
-      "newline": "\n",
-      "raw_data": {
-        "basename": "shard.00000.csv",
-        "bytes": 1048523,
-        "hashes": {
-          "sha1": "39f6ea99d882d3652e34fe5bd4682454664efeda",
-          "xxh3_64": "ea1572efa0207ff6"
-        }
-      },
-      "raw_meta": {
-        "basename": "shard.00000.csv.meta",
-        "bytes": 77486,
-        "hashes": {
-          "sha1": "8874e88494214b45f807098dab9e55d59b6c4aec",
-          "xxh3_64": "3b1837601382af2c"
-        }
-      },
-      "samples": 19315,
-      "separator": ",",
-      "size_limit": 1048576,
-      "version": 2,
-      "zip_data": {
-        "basename": "shard.00000.csv.zstd",
-        "bytes": 197040,
-        "hashes": {
-          "sha1": "021d288a317ae0ecacba8a1b985ee107f966710d",
-          "xxh3_64": "5daa4fd69d3578e4"
-        }
-      },
-      "zip_meta": {
-        "basename": "shard.00000.csv.meta.zstd",
-        "bytes": 60981,
-        "hashes": {
-          "sha1": "f2a35f65279fbc45e8996fa599b25290608990b2",
-          "xxh3_64": "7c38dee2b3980deb"
-        }
-      }
-    }
-'''
 
 
 class XSVReader(SplitReader):
