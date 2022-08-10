dc1ce77ba442280444202b97f74ecd4bf760ad2d
James Knighton <iamknighton@gmail.com>
2022-08-10 12:32:59 -0700

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -6,17 +6,12 @@ import setuptools
 from setuptools import setup
 
 
-def package_files(prefix: str, directory: str, extension: str):
-    # from https://stackoverflow.com/a/36693250
-    paths = []
-    for (path, _, filenames) in os.walk(os.path.join(prefix, directory)):
-        for filename in filenames:
-            if filename.endswith(extension):
-                paths.append(os.path.relpath(os.path.join(path, filename), prefix))
-    return paths
-
-
-data_files = package_files("mcontrib", "algorithms", ".json")
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+]
 
 install_requires = [
     "boto3==1.24.37",
@@ -45,22 +40,13 @@ extra_deps = {
 
 setup(
     name="streaming",
-    version="0.0.0",
+    version="0.1.0",
     author="MosaicML",
     author_email="team@mosaicml.com",
     description='Streaming datasets',
     url="https://github.com/mosaicml/streaming/",
-    include_package_data=True,
-    package_data={
-        "mcontrib": data_files,
-    },
     packages=setuptools.find_packages(exclude=["tests*"]),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
+    classifiers=classifiers,
     install_requires=install_requires,
     extras_require=extra_deps,
     python_requires=">=3.7",
