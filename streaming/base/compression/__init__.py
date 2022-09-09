ad5dc61f0f0ce235d8441af02df5c63057a42da8
Karan Jariwala <karankjariwala@gmail.com>
2022-09-09 09:14:10 -0700

    Fixed linting

--- streaming/base/compression/__init__.py
+++ streaming/base/compression/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2022 MosaicML Composer authors
+# Copyright 2022 MosaicML Streaming authors
 # SPDX-License-Identifier: Apache-2.0
 
 from .compression import (compress, decompress, get_compression_extension, get_compressions,
