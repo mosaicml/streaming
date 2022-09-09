e42707d75512fc4f4abb671fe17862747ef39c8b
James Knighton <iamknighton@gmail.com>
2022-09-09 15:50:56 -0700

    Single quotes.

--- setup.py
+++ setup.py
@@ -13,28 +13,28 @@ classifiers = [
 ]
 
 install_requires = [
-    "boto3==1.24.37",
-    "Brotli==1.0.9",
-    "datasets==2.4.0",
-    "matplotlib==3.5.2",
-    "paramiko==2.11.0",
-    "python-snappy==0.6.1",
-    "torch>=1.10,<2",
-    "torchtext>=0.10",
-    "torchvision>=0.10",
-    "tqdm==4.64.0",
-    "transformers==4.21.3",
-    "xxhash==3.0.0",
-    "zstd==1.5.2.5",
+    'boto3==1.24.37',
+    'Brotli==1.0.9',
+    'datasets==2.4.0',
+    'matplotlib==3.5.2',
+    'paramiko==2.11.0',
+    'python-snappy==0.6.1',
+    'torch>=1.10,<2',
+    'torchtext>=0.10',
+    'torchvision>=0.10',
+    'tqdm==4.64.0',
+    'transformers==4.21.3',
+    'xxhash==3.0.0',
+    'zstd==1.5.2.5',
 ]
 
 extra_deps = {}
 
 extra_deps['dev'] = [
-    "docformatter==1.4",
-    "pytest==7.1.2",
-    "toml==0.10.2",
-    "yamllint==1.26.3",
+    'docformatter==1.4',
+    'pytest==7.1.2',
+    'toml==0.10.2',
+    'yamllint==1.26.3',
     'pre-commit>=2.18.1,<3',
 ]
 
