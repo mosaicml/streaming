ee0696518b258169dcf43c06c2267a3b70dd8b28
James Knighton <iamknighton@gmail.com>
2022-09-14 20:41:51 -0700

    Fix docs docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -347,8 +347,8 @@ def _auto_rst_for_module(module: types.ModuleType, exclude_members: List[Any]) -
     classes.sort(key=lambda x: x[0])
     attributes.sort(key=lambda x: x[0])
 
-    for category, category_name in ((functions, 'Functions'), (classes, 'Classes'), (exceptions,
-                                                                                     'Exceptions')):
+    for category, category_name in ((functions, 'Functions'), (classes, 'Classes'),
+                                    (exceptions, 'Exceptions')):
         sphinx_lines = []
         for item_name, _ in category:
             sphinx_lines.append(f'      {item_name}')
@@ -371,7 +371,8 @@ def _auto_rst_for_module(module: types.ModuleType, exclude_members: List[Any]) -
 
 
 def _modules_to_rst() -> List[types.ModuleType]:
-    """Return the list of modules for which to generate API reference rst files."""
+    """Return the list of modules for which to generate API reference rst
+    files."""
 
     document_modules: List[types.Module] = [
         streaming,
@@ -388,8 +389,8 @@ def _modules_to_rst() -> List[types.ModuleType]:
 def _generate_rst_files_for_modules() -> None:
     """Generate .rst files for each module to include in the API reference.
 
-    These files contain the module docstring followed by tables listing all
-    the functions, classes, etc.
+    These files contain the module docstring followed by tables listing
+    all the functions, classes, etc.
     """
     docs_dir = os.path.abspath(os.path.dirname(__file__))
     module_rst_save_dir = os.path.join(docs_dir, 'api_reference')
@@ -496,7 +497,8 @@ def linkcode_resolve(domain: str, info: Dict[str, str]):
             # in _determine_lineno_of_attribute
             pass
     if lineno is None:
-        log.debug(f'Could not determine source line number for {module_name}.{obj_name_in_module}.')
+        log.debug(
+            f'Could not determine source line number for {module_name}.{obj_name_in_module}.')
         return None
     # Format the link
     filename = module_name.replace('.', '/')
@@ -548,7 +550,8 @@ class PatchedHTMLTranslator(HTML5Translator):
         self.body.append(self.starttag(node, 'a', '', **atts))
 
         if node.get('secnumber'):
-            self.body.append(('%s' + self.secnumber_suffix) % '.'.join(map(str, node['secnumber'])))
+            self.body.append(
+                ('%s' + self.secnumber_suffix) % '.'.join(map(str, node['secnumber'])))
 
 
 def setup(app: sphinx.application.Sphinx):
