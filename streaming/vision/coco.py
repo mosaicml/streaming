c32b0a6216256d970963c89297a463413b917094
James Knighton <iamknighton@gmail.com>
2022-09-15 16:23:06 -0700

    99 col.

--- streaming/vision/coco.py
+++ streaming/vision/coco.py
@@ -12,19 +12,25 @@ class COCO(Dataset):
 
     Args:
         local (str): Local filesystem directory where dataset is cached during operation.
-        remote (str, optional): Remote directory (S3 or local filesystem) where dataset is stored. Default: ``None``.
-        split (str, optional): The dataset split to use, either 'train' or 'val'. Default: ``None``.
-        shuffle (bool, optional): Whether to shuffle the train samples in this dataset. Default: ``True``.
-        transform (callable, optional): A function/transform that takes in an image and bboxes and returns a transformed
-            version. Default: ``None``.
-        prefetch (int, optional): Target number of samples remaining to prefetch while iterating. Default: ``100_000``.
-        keep_zip (bool, optional): Whether to keep or delete the compressed file when decompressing downloaded shards.
-            If set to None, keep iff remote == local. Default: ``None``.
-        retry (int, optional): Number of download re-attempts before giving up. Default: ``2``.
-        timeout (float, optional): Number of seconds to wait for a shard to download before raising an exception.
-            Default: ``60``.
-        hash (str, optional): Hash or checksum algorithm to use to validate shards. Default: ``None``.
-        batch_size (int, optional): Batch size that will be used on each device's DataLoader. Default: ``None``.
+        remote (str, optional): Remote directory (S3 or local filesystem) where dataset is stored.
+            Defaults to ``None``.
+        split (str, optional): The dataset split to use, either 'train' or 'val'. Defaults to
+            ``None``.
+        shuffle (bool, optional): Whether to shuffle the train samples in this dataset. Defaults to
+            ``True``.
+        transform (callable, optional): A function/transform that takes in an image and bboxes and
+            returns a transformed version. Defaults to ``None``.
+        prefetch (int, optional): Target number of samples remaining to prefetch while iterating.
+            Defaults to ``100_000``.
+        keep_zip (bool, optional): Whether to keep or delete the compressed file when decompressing
+            downloaded shards. If set to None, keep iff remote == local. Defaults to ``None``.
+        retry (int, optional): Number of download re-attempts before giving up. Defaults to ``2``.
+        timeout (float, optional): Number of seconds to wait for a shard to download before raising
+            an exception. Defaults to ``60``.
+        hash (str, optional): Hash or checksum algorithm to use to validate shards. Defaults to
+            ``None``.
+        batch_size (int, optional): Batch size that will be used on each device's DataLoader.
+            Defaults to ``None``.
     """
 
     def __init__(self,
