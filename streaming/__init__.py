4b1530ddc77d097807730ba28b97d59a35c23913
Karan Jariwala <karankjariwala@gmail.com>
2022-09-19 11:14:22 -0700

    Update package version code for unification

--- streaming/__init__.py
+++ streaming/__init__.py
@@ -1,6 +1,7 @@
 # Copyright 2022 MosaicML Streaming authors
 # SPDX-License-Identifier: Apache-2.0
 
+from streaming._version import __version__
 from streaming.base import (CSVWriter, Dataset, JSONWriter, LocalDataset, MDSWriter, TSVWriter,
                             XSVWriter)
 
---
+++ streaming/_version.py
@@ -0,0 +1,6 @@
+# Copyright 2022 MosaicML Streaming authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""The Streaming Version."""
+
+__version__ = '0.0.1a'
