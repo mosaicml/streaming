358947ff0f2c18defea2d42e239bde1a46222e5e
James Knighton <iamknighton@gmail.com>
2022-10-07 23:49:00 +0000

    Fix spacing.

--- streaming/text/convert/enwiki/tfrecord/make_train_parallel.py
+++ streaming/text/convert/enwiki/tfrecord/make_train_parallel.py
@@ -22,13 +22,13 @@ def main(arg):
             python3 create_pretraining_data.py \
                 --input_file {input_files} \
                 --output_file {output_file} \
-               --vocab_file vocab.txt \
-               --do_lower_case True \
-               --max_seq_length 512 \
-               --max_predictions_per_seq 76 \
-               --masked_lm_prob 0.15 \
-               --random_seed 12345 \
-               --dupe_factor 10 &
+                --vocab_file vocab.txt \
+                --do_lower_case True \
+                --max_seq_length 512 \
+                --max_predictions_per_seq 76 \
+                --masked_lm_prob 0.15 \
+                --random_seed 12345 \
+                --dupe_factor 10 &
         '''
         os.system(command)
 
