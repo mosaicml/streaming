84c23eab402eb4cde139cbef39566f15f266bb7f
Karan Jariwala <karankjariwala@gmail.com>
2022-10-24 22:26:24 -0700

    Add Distributed test and supported multi device unittest (#57)
    * Add Distributed test and supported multi device unittest
    
    * Removed unnecessary comments
    
    * Update socket parameters and removed unnecessary comments

--- streaming/base/distributed.py
+++ streaming/base/distributed.py
@@ -4,6 +4,11 @@
 """Helper methods to get the distributed attributes."""
 
 import os
+from typing import List, TypeVar, cast
+
+import torch.distributed as dist
+
+TObj = TypeVar('TObj')
 
 __all__ = ['get_global_rank', 'get_local_rank', 'get_local_world_size', 'get_world_size']
 
@@ -42,3 +47,30 @@ def get_local_world_size() -> int:
         int: The local world size.
     """
     return int(os.environ.get('LOCAL_WORLD_SIZE', 1))
+
+
+def all_gather_object(obj: TObj) -> List[TObj]:
+    """Collect a pickle-able object from each rank and return a list of these objects.
+
+    .. seealso:: :func:`torch.distributed.all_gather_object`
+
+    Args:
+        obj (TObj): Object to be gathered.
+
+    Returns:
+        List[TObj]: A list of objects indexed by rank.
+    """
+    if dist.is_available() and dist.is_initialized():
+        obj_gather_list = [0 for _ in range(get_world_size())]
+        dist.all_gather_object(obj_gather_list, obj)
+        # torch.distributed will replace the None's in obj_gather_list with the gathered objects on rank 0
+        # or will just be None on non-rank-0
+        return cast(List[TObj], obj_gather_list)
+    world_size = get_world_size()
+    if world_size == 1:
+        return [obj]
+    raise RuntimeError(''.join([
+        f'The world_size({world_size}) > 1, but the distributed package is not available ',
+        'or has not been initialized. Please check you have initialized the distributed ',
+        'runtime and that PyTorch has been built with distributed support.'
+    ]))
---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+# Copyright 2022 MosaicML Streaming authors
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Any
+
+import pytest
+
+
+# Override of pytest "runtest" for DistributedTest class
+# This hook is run before the default pytest_runtest_call
+@pytest.hookimpl(tryfirst=True)  # pyright: ignore
+def pytest_runtest_call(item: Any):
+    # Launch a custom function for distributed tests
+    if getattr(item.cls, 'is_dist_test', False):
+        dist_test_class = item.cls()
+        dist_test_class._run_test(item._request)
+        item.runtest = lambda: True  # Dummy function so test is not run twice
