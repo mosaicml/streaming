092d14c69ecb1ab9aa7ab0d739bf8e6099fad77b
James Knighton <iamknighton@gmail.com>
2022-11-05 00:40:34 +0000

    Update text datasets.

--- streaming/text/enwiki.py
+++ streaming/text/enwiki.py
@@ -16,38 +16,46 @@ class EnWiki(Dataset):
     """Implementation of the English Wikipedia 2020-01-01 streaming dataset.
 
     Args:
-        local (str): Local filesystem directory where dataset is cached during operation.
-        remote (str, optional): Remote directory (S3 or local filesystem) where dataset is stored.
-            Defaults to ``None``.
-        split (str, optional): The dataset split to use, either 'train' or 'val'. Defaults to
+        local (str): Local dataset directory where shards are cached by split.
+        remote (str, optional): Download shards from this remote path or directory. If None, this
+            rank and worker's partition of the dataset must all exist locally. Defaults to
             ``None``.
+        split (str, optional): Which dataset split to use, if any. Defaults to ``None``.
         shuffle (bool): Whether to iterate over the samples in randomized order. Defaults to
-            ``True``.
-        prefetch (int, optional): Target number of samples remaining to prefetch while iterating.
-            Defaults to ``100_000``.
-        keep_zip (bool, optional): Whether to keep or delete the compressed file when decompressing
-            downloaded shards. If set to None, keep iff remote is local. Defaults to ``None``.
-        retry (int): Number of download re-attempts before giving up. Defaults to ``2``.
-        timeout (float): Number of seconds to wait for a shard to download before raising
-            an exception. Defaults to ``60``.
-        hash (str, optional): Hash or checksum algorithm to use to validate shards. Defaults to
+            ``False``.
+        predownload (int, optional): Target number of samples ahead to download the shards of while
+            iterating. Defaults to ``100_000``.
+        keep_zip (bool, optional): Whether to keep or delete the compressed file when
+            decompressing downloaded shards. If set to None, keep iff remote is local. Defaults to
             ``None``.
-        batch_size (int, optional): Hint the batch size that will be used on each device's DataLoader.
-            Defaults to ``None``.
+        download_retry (int): Number of download re-attempts before giving up. Defaults to ``2``.
+        download_timeout (float): Number of seconds to wait for a shard to download before raising
+            an exception. Defaults to ``60``.
+        validate_hash (str, optional): Optional hash or checksum algorithm to use to validate
+            shards. Defaults to ``None``.
+        shuffle_seed (int, optional): Seed for shuffling, or ``None`` for random seed. Defaults to
+            ``None``.
+        shuffle_world_size (int, optional): Canonical world size for shuffling. Defaults to
+            ``None``, which is interpreted as the world size of the initial run.
+        batch_size (int, optional): Batch size of its DataLoader, which affects how the dataset is
+            partitioned over the workers. Defaults to ``None``.
     """
 
     def __init__(self,
                  local: str,
                  remote: Optional[str] = None,
                  split: Optional[str] = None,
-                 shuffle: bool = True,
-                 prefetch: Optional[int] = 100_000,
+                 shuffle: bool = False,
+                 predownload: Optional[int] = 100_000,
                  keep_zip: Optional[bool] = None,
-                 retry: int = 2,
-                 timeout: float = 60,
-                 hash: Optional[str] = None,
-                 batch_size: Optional[int] = None) -> None:
-        super().__init__(local, remote, split, shuffle, prefetch, keep_zip, retry, timeout, hash,
+                 download_retry: int = 2,
+                 download_timeout: float = 60,
+                 validate_hash: Optional[str] = None,
+                 shuffle_seed: Optional[int] = None,
+                 shuffle_world_size: Optional[int] = None,
+                 batch_size: Optional[int] = None):
+        super().__init__(local, remote, split, shuffle, predownload, keep_zip, download_retry,
+                         download_timeout, validate_hash, shuffle_seed, shuffle_world_size,
                          batch_size)
         self.field_dtypes = {
             'input_ids': np.int32,
