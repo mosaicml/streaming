02dbe2c644147b9f274670bf40642fa2ebec0e49
Karan Jariwala <karankjariwala@gmail.com>
2022-11-30 22:20:48 -0800

    Fixed lint

--- scripts/hashing/bench.py
+++ scripts/hashing/bench.py
@@ -9,7 +9,7 @@ from typing import Iterator
 
 import numpy as np
 
-from streaming.base.hashing.hashing import get_hash, get_hashes
+from streaming.base.hashing import get_hash, get_hashes
 
 
 def parse_args() -> Namespace:
