9deed5ccfd8b6f367d33ea99302e418f5687260e
James Knighton <iamknighton@gmail.com>
2022-12-22 01:19:57 +0000

    Just the FileLock, please.

--- streaming/base/dataset.py
+++ streaming/base/dataset.py
@@ -226,11 +226,12 @@ class StreamingDataset(IterableDataset):
         # data to be picked up by __iter__().
         self._resume_shm = None
 
-        # Create the setup for the worker barrier.
-        self._worker_barrier_filelock_path = os.path.join(os.path.sep, 'tmp', 'streaming',
-                                                          self._prefix, 'barrier_filelock')
-        self._worker_barrier_shm_path = f'{self._prefix}_barrier'
-        self._worker_barrier: SharedBarrier
+        # Create the shared memory-backed worker barrier, without its lock, which is unpickleable.
+        worker_barrier_filelock_path = os.path.join(os.path.sep, 'tmp', 'streaming', self._prefix,
+                                                    'barrier_filelock')
+        worker_barrier_shm_path = f'{self._prefix}_barrier'
+        self._worker_barrier = SharedBarrier(worker_barrier_filelock_path, worker_barrier_shm_path)
+        del self._worker_barrier.lock
 
         # Partition state.
         self._partition_state = None
@@ -707,11 +708,10 @@ class StreamingDataset(IterableDataset):
         Returns:
             Iterator[Dict[str, Any]]: Each sample.
         """
-        # Lazily create the worker barrier, because it contains a FileLock, which contains a
-        # threading Lock, which is unpickleable.
-        if not hasattr(self, '_worker_barrier'):
-            self._worker_barrier = SharedBarrier(self._worker_barrier_filelock_path,
-                                                 self._worker_barrier_shm_path)
+        # Lazily create the worker barrier's FileLock, which contains a threading Lock, which is
+        # unpickleable.
+        if not hasattr(self._worker_barrier, 'lock'):
+            self._worker_barrier.lock = FileLock(self._worker_barrier.filelock_path)
 
         # Exit the thread that is downloading the shards for last epoch, if it exists.
         if self._partition_state:
