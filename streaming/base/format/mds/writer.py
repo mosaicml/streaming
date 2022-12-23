d15926d0e19374a7f6d9675d778cb83dea2eb736
Daniel King <daniel@mosaicml.com>
2022-12-23 13:45:06 -0800

    lint

--- streaming/base/format/mds/writer.py
+++ streaming/base/format/mds/writer.py
@@ -9,7 +9,8 @@ from typing import Any, Dict, List, Optional
 import numpy as np
 
 from streaming.base.format.base.writer import JointWriter
-from streaming.base.format.mds.encodings import get_mds_encoded_size, is_mds_encoding, mds_encode, get_mds_encodings
+from streaming.base.format.mds.encodings import (get_mds_encoded_size, get_mds_encodings,
+                                                 is_mds_encoding, mds_encode)
 
 __all__ = ['MDSWriter']
 
@@ -46,7 +47,9 @@ class MDSWriter(JointWriter):
         for name in sorted(columns):
             encoding = columns[name]
             if not is_mds_encoding(encoding):
-                raise TypeError(f'MDSWriter passed column "{name}" with encoding "{encoding}" is unsupported. Supported encodings are {get_mds_encodings()}')
+                raise TypeError(
+                    f'MDSWriter passed column "{name}" with encoding "{encoding}" is unsupported. Supported encodings are {get_mds_encodings()}'
+                )
             size = get_mds_encoded_size(encoding)
             self.column_names.append(name)
             self.column_encodings.append(encoding)
