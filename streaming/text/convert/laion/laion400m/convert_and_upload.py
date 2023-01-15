9377a8ec14726c0b4fd13ee57e9e4edea28a0237
James Knighton <iamknighton@gmail.com>
2023-01-15 06:57:24 +0000

    2022 -> 2023.

--- streaming/text/convert/laion/laion400m/convert_and_upload.py
+++ streaming/text/convert/laion/laion400m/convert_and_upload.py
@@ -1,4 +1,4 @@
-# Copyright 2022 MosaicML Streaming authors
+# Copyright 2023 MosaicML Streaming authors
 # SPDX-License-Identifier: Apache-2.0
 
 """Convert and upload LAION-400M parquet shards."""
