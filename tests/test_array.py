d455f8b6005e9f4d724a7954c901f8ed4af009c4
James Knighton <iamknighton@gmail.com>
2023-01-20 01:04:30 -0800

    Use pytest fixtures, beef up tests.

--- tests/test_array.py
+++ tests/test_array.py
@@ -1,6 +1,7 @@
 # Copyright 2023 MosaicML Streaming authors
 # SPDX-License-Identifier: Apache-2.0
 
+import pytest
 from typing import List, Union
 
 import numpy as np
@@ -24,44 +25,59 @@ class Range(Array):
         return idx
 
 
-size = 100
-theirs = np.arange(size)
-mine = Range(size)
-
-
-def check(i: Union[int, slice, List[int], NDArray[np.int64]]):
+def check(np_arr: NDArray[np.int64], my_arr: Range, i: Union[int, slice, List[int],
+          NDArray[np.int64]]):
     try:
-        t = theirs[i]
+        t = np_arr[i]
     except:
         t = None
     if isinstance(t, np.ndarray):
         t = t.tolist()
 
     try:
-        m = mine[i]
+        m = my_arr[i]
     except:
         m = None
 
     assert t == m
 
 
-def test_int_from_front():
-    for i in range(size):
-        check(i)
+@pytest.fixture
+def np_arange():
+    return np.arange(100)
 
 
-def test_int_from_back():
-    for i in range(-size, 0):
-        check(i)
+@pytest.fixture
+def my_arange():
+    return Range(100)
 
 
-def test_int_out_of_range():
-    for i in range(-size * 4, size * 4, 10):
-        check(i)
+def test_int_from_front(np_arange, my_arange):
+    for i in range(100):
+        check(np_arange, my_arange, i)
 
 
-def each_slice():
-    yield from [
+def test_int_from_back(np_arange, my_arange):
+    for i in range(-100, 0):
+        check(np_arange, my_arange, i)
+
+
+def test_int_out_of_range(np_arange, my_arange):
+    for i in range(-100 * 4, 100 * 4, 10):
+        check(np_arange, my_arange, i)
+
+
+@pytest.mark.parametrize('start', [-142, -100, -99, -42, -1, 0, 42, 99, 100, 142])
+@pytest.mark.parametrize('stop', [-142, -100, -99, -42, -1, 0, 42, 99, 100, 142])
+@pytest.mark.parametrize('step', [-3, -1, 1, 3])
+def test_slice(start, stop, step, np_arange, my_arange):
+    i = slice(start, stop, step)
+    check(np_arange, my_arange, i)
+
+
+@pytest.fixture
+def slices():
+    return [
         slice(0),
         slice(0, 0),
         slice(0, 1),
@@ -82,71 +98,52 @@ def each_slice():
         slice(1338, 42, -5),
         slice(-1337, 42, 5),
         slice(-1337, 42, -5),
-        slice(42, 101, -1),
-        slice(42, 100, -1),
-        slice(42, 99, -1),
-        slice(42, 42, -1),
-        slice(42, 1, -1),
-        slice(42, 0, -1),
-        slice(42, -1, -1),
-        slice(42, -99, -1),
-        slice(42, -100, -1),
-        slice(42, -101, -1),
-        slice(42, 101, -3),
-        slice(42, 100, -3),
-        slice(42, 99, -3),
-        slice(42, 1, -3),
-        slice(42, 0, -3),
-        slice(42, -1, -3),
-        slice(42, -99, -3),
-        slice(42, -100, -3),
-        slice(42, -101, -3),
     ]
 
 
-def test_slice():
-    for i in each_slice():
-        check(i)
+def test_specific_slice(np_arange, my_arange, slices):
+    for i in slices:
+        check(np_arange, my_arange, i)
 
 
-def test_list():
-    for i in each_slice():
-        i = mine._each_slice_index(i)
+def test_list(np_arange, my_arange, slices):
+    for i in slices:
+        i = my_arange._each_slice_index(i)
         i = list(i)
-        check(i)
+        check(np_arange, my_arange, i)
 
 
-def test_array():
-    for i in each_slice():
-        i = mine._each_slice_index(i)
+def test_array(np_arange, my_arange, slices):
+    for i in slices:
+        i = my_arange._each_slice_index(i)
         i = list(i)
         i = np.array(i, dtype=np.int64)
-        check(i)
+        check(np_arange, my_arange, i)
 
 
-def test_array_2d():
+def test_array_2d(np_arange, my_arange):
     i = np.arange(4)
     i = i * 3 + 7
     i = i.reshape(2, 2)
-    check(i)
+    check(np_arange, my_arange, i)
 
 
-def test_array_3d():
+def test_array_3d(np_arange, my_arange):
     i = np.arange(8)
     i = i * 3 + 7
     i = i.reshape(2, 2, 2)
-    check(i)
+    check(np_arange, my_arange, i)
 
 
-def test_list_2d():
+def test_list_2d(np_arange, my_arange):
     i = np.arange(4)
     i = i * 3 + 7
     i = i.reshape(2, 2)
-    assert mine[i.tolist()] == theirs[i].tolist()
+    assert my_arange[i.tolist()] == np_arange[i].tolist()
 
 
-def test_list_3d():
+def test_list_3d(np_arange, my_arange):
     i = np.arange(8)
     i = i * 3 + 7
     i = i.reshape(2, 2, 2)
-    assert mine[i.tolist()] == theirs[i].tolist()
+    assert my_arange[i.tolist()] == np_arange[i].tolist()
