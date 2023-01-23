f000c2a53897b22f5e5b99b61dc46544034a25e5
James Knighton <iamknighton@gmail.com>
2023-01-23 09:27:20 -0800

    Add scalar MDS encodings (data types). (#130)

--- streaming/base/format/mds/encodings.py
+++ streaming/base/format/mds/encodings.py
@@ -88,6 +88,97 @@ class Int(Encoding):
         return int(np.frombuffer(data, np.int64)[0])
 
 
+class Scalar(Encoding):
+    """Store scalar."""
+
+    def __init__(self, dtype: type) -> None:
+        self.dtype = dtype
+        self.size = self.dtype().nbytes
+
+    def encode(self, obj: Any) -> bytes:
+        return self.dtype(obj).tobytes()
+
+    def decode(self, data: bytes) -> Any:
+        return np.frombuffer(data, self.dtype)[0]
+
+
+class UInt8(Scalar):
+    """Store uint8."""
+
+    def __init__(self):
+        super().__init__(np.uint8)
+
+
+class UInt16(Scalar):
+    """Store uint16."""
+
+    def __init__(self):
+        super().__init__(np.uint16)
+
+
+class UInt32(Scalar):
+    """Store uint32."""
+
+    def __init__(self):
+        super().__init__(np.uint32)
+
+
+class UInt64(Scalar):
+    """Store uint64."""
+
+    def __init__(self):
+        super().__init__(np.uint64)
+
+
+class Int8(Scalar):
+    """Store int8."""
+
+    def __init__(self):
+        super().__init__(np.int8)
+
+
+class Int16(Scalar):
+    """Store int16."""
+
+    def __init__(self):
+        super().__init__(np.int16)
+
+
+class Int32(Scalar):
+    """Store int32."""
+
+    def __init__(self):
+        super().__init__(np.int32)
+
+
+class Int64(Scalar):
+    """Store int64."""
+
+    def __init__(self):
+        super().__init__(np.int64)
+
+
+class Float16(Scalar):
+    """Store float16."""
+
+    def __init__(self):
+        super().__init__(np.float16)
+
+
+class Float32(Scalar):
+    """Store float32."""
+
+    def __init__(self):
+        super().__init__(np.float32)
+
+
+class Float64(Scalar):
+    """Store float64."""
+
+    def __init__(self):
+        super().__init__(np.float64)
+
+
 class PIL(Encoding):
     """Store PIL image raw.
 
@@ -174,6 +265,17 @@ _encodings = {
     'bytes': Bytes,
     'str': Str,
     'int': Int,
+    'uint8': UInt8,
+    'uint16': UInt16,
+    'uint32': UInt32,
+    'uint64': UInt64,
+    'int8': Int8,
+    'int16': Int16,
+    'int32': Int32,
+    'int64': Int64,
+    'float16': Float16,
+    'float32': Float32,
+    'float64': Float64,
     'pil': PIL,
     'jpeg': JPEG,
     'png': PNG,
