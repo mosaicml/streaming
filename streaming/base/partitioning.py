89870c0168eeaa6b63f5ef9bd50d1fd755776783
James Knighton <iamknighton@gmail.com>
2023-01-25 01:53:27 -0800

    Fix (improve commenting).

--- streaming/base/partitioning.py
+++ streaming/base/partitioning.py
@@ -110,74 +110,78 @@ def get_partitions_fast(dataset_size: int,
         dataset_size (int): Dataset size.
         num_canonical_nodes (int): Number of canonical nodes.
         num_physical_nodes (int): Number of physical nodes.
-        node_devices (int): Number of ranks per node.
-        device_workers (int): Number of worker partitions per rank.
+        node_devices (int): Number of devices per node.
+        device_workers (int): Number of worker partitions per device.
         device_batch_size (int): Batch size of its DataLoader, which affects how the dataset is
             partitioned over the workers. Defaults to ``1``.
         drop_first (int): Number of samples seen already, which are dropped. Defaults to ``0``.
 
     Returns:
-        NDArray[np.int64]: Partitions of shape (physical nodes x ranks per node x workers per rank x batches per worker x device batch size).
+        NDArray[np.int64]: Partitions of shape (physical nodes x devices per node x workers per
+            device x batches per worker x device batch size).
     """
     if num_canonical_nodes % num_physical_nodes and num_physical_nodes % num_canonical_nodes:
         raise ValueError('One of {canonical nodes, physical nodes} must be evenly divisible by ' +
                          'the other.')
 
-    devices = num_physical_nodes * node_devices
-    device_samples = math.ceil(dataset_size / devices)
+    # Calculate samples per device and padding.
+    num_devices = num_physical_nodes * node_devices
+    device_samples = math.ceil(dataset_size / num_devices)
     worker_batches = math.ceil(device_samples / (device_workers * device_batch_size))
     padded_device_samples = device_workers * worker_batches * device_batch_size
 
+    # Calculate starts and steps in terms of canonical nodes.
     node_starts = dataset_size * np.arange(num_canonical_nodes) // num_canonical_nodes
-    per_node_device_starts = np.arange(node_devices)
+    node_device_starts = np.arange(node_devices)
     step = node_devices
 
+    # If we are training on a reduced number of nodes, scale starts and steps accordingly.
     if num_canonical_nodes < num_physical_nodes:
         node_ratio = num_physical_nodes // num_canonical_nodes
         node_starts = np.tile(node_starts, node_ratio)
         node_starts += np.arange(node_ratio).repeat(num_canonical_nodes)
-        per_node_device_starts *= node_ratio
+        node_device_starts *= node_ratio
         step *= node_ratio
 
-    # starts shape -> (num nodes x ranks per node x 1)
-    starts = node_starts.reshape(-1, 1, 1) + per_node_device_starts.reshape(1, -1, 1)
-    # indices shape -> (1 x 1 x padded device samples)
+    # Sample IDs: (nodes x node devices x padded device samples).
+    starts = node_starts.reshape(-1, 1, 1) + node_device_starts.reshape(1, -1, 1)
     indices = np.arange(padded_device_samples).reshape(1, 1, -1)
-    # ids shape -> (num_canonical_nodes x ranks per node x padded device samples)
     ids = starts + indices * step
 
+    # If we are training on an increased number of nodes, interleave canonical devices onto
+    # physical devices so that the global order of the samples is preserved.
     if num_physical_nodes < num_canonical_nodes:
-        # reshape the ids so that the order of the samples is preserved based on the provided number
-        # of canonical nodes
         node_ratio = num_canonical_nodes // num_physical_nodes
         ids = ids.reshape(node_ratio, num_physical_nodes, node_devices, -1)
         ids = ids.transpose(1, 3, 2, 0)
         ids = ids.reshape(num_physical_nodes, -1, node_devices)
         ids = ids.transpose(0, 2, 1)
         ids = ids[:, :, :padded_device_samples]
+    # Sample IDs: (physical nodes x node devices x padded device samples).
 
-    # ids shape -> (physical nodes x ranks per node x padded device samples)
+    # Reassign sample IDs that need to be pressent to keep samples balanced across devices, but
+    # would extend past the end of the dataset.
     second_to_last = ids[:, :, device_samples - 2]
     last = ids[:, :, device_samples - 1]
-    # duplicates valid samples at the end, so that each device has the same number of samples
     ids[:, :, device_samples - 1] = np.where(last < dataset_size, last, second_to_last)
-    # drops all samples past the end of the dataset
+    # Drop all unwanted sample IDs hallucinated past the end of the dataset.
     ids[:, :, device_samples:] = -1
 
+    # If are mid-epoch, drop the first drop_first samples by flattening into the order that samples
+    # wouldl be seen and clipping the samples from the left.
     if drop_first:
-        # drop the first drop_first samples by flattening into the order that samples would be seen and
-        # clipping the samples from the left
         ids = ids.transpose(2, 0, 1)
         ids = ids.flatten()
         ids[:-drop_first] = ids[drop_first:]
         ids[-drop_first:] = -1
         ids = ids.reshape(padded_device_samples, num_physical_nodes, node_devices)
-        # return to original ids shape of (physical nodes x ranks per node x padded device samples)
-        ids = ids.transpose(1, 2, 0)
+        ids = ids.transpose(1, 2, 0)  # Return to original shape.
 
+    # Partition samples per device across each device's workers and workers' batches.
     ids = ids.reshape(num_physical_nodes, node_devices, worker_batches, device_workers,
                       device_batch_size)
-    # ids shape -> (physical nodes x ranks per node x workers per rank x worker batches x device batch size)
+    # Sample IDs: (physical nodes x devices per node x workers per device x worker batches x device
+    # batch size)
     return ids.transpose(0, 1, 3, 2, 4)
 
 
