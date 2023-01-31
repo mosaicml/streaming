686c80d951e07995171db596b23b3ad6b9210482
James Knighton <iamknighton@gmail.com>
2023-01-30 18:16:35 -0800

    get_shuffle_med.

---
+++ scripts/shuffle/bench.py
@@ -0,0 +1,125 @@
+# Copyright 2023 MosaicML Streaming authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Command-line tool to benchmark StreamingDataset shuffling performance."""
+
+import math
+from argparse import ArgumentParser, Namespace
+from time import time
+from typing import Callable
+
+import numpy as np
+from numpy.typing import NDArray
+
+from streaming.base.shuffle import get_shuffle_med, get_shuffle_slow
+
+
+def parse_args() -> Namespace:
+    """Parse command-line arguments.
+
+    Returns:
+        Namespace: Command-line arguments.
+    """
+    args = ArgumentParser()
+    args.add_argument('--num_canonical_nodes', type=int, default=1)
+    args.add_argument('--seed', type=int, default=9186)
+    args.add_argument('--epoch', type=int, default=0)
+    args.add_argument('--timeout', type=float, default=120)
+    args.add_argument('--samples_per_shard', type=float, default=50_000)
+    args.add_argument('--min_power', type=int, default=10)
+    args.add_argument('--max_power', type=int, default=34)
+    args.add_argument('--power_interval', type=int, default=4)
+    return args.parse_args()
+
+
+class Caller(object):
+    """Partial function application with timeouts.
+
+    Args:
+        func (Callable): The wrapped function to time.
+        num_canonical_nodes (int): Shuffle canonical nodes.
+        seed (int): Shuffle seed.
+        epoch (int): Shuffle epoch.
+        timeout (float): Maximum time elapsed before it skips benchmarking the wrapped method.
+    """
+
+    def __init__(self, func: Callable, num_canonical_nodes: int, seed: int, epoch: int,
+                 timeout: float) -> None:
+        self.func = func
+        self.num_canonical_nodes = num_canonical_nodes
+        self.seed = seed
+        self.epoch = epoch
+        self.timeout = timeout
+        self.do = True
+
+    def __call__(self, shard_sizes: NDArray[np.int64]) -> float:
+        """Call the contained function with the given shard sizes.
+
+        Args:
+            shard_sizes (NDArray[np.int64]): Size of each shard.
+
+        Returns:
+            float: Time to shuffle.
+        """
+        if not self.do:
+            return 0
+        start = time()
+        self.func(shard_sizes, self.num_canonical_nodes, self.seed, self.epoch)
+        elapsed = time() - start
+        if self.timeout < elapsed:
+            self.do = False
+        return elapsed
+
+
+def get_shard_sizes(dataset_size: int, samples_per_shard: float) -> NDArray[np.int64]:
+    """Calculate realistic shard sizes given samples and samples/shard.
+
+    Args:
+        dataset_size (int): Total samples in the dataset.
+        samples_per_shard (float): Average shard size in samples.
+
+    Returns:
+        NDArray[np.int64]: The sample size of each shard.
+    """
+    num_shards = math.ceil(dataset_size / samples_per_shard)
+    shard_sizes = num_shards * [samples_per_shard]
+    shard_sizes[-1] -= num_shards * samples_per_shard - dataset_size
+    assert sum(shard_sizes) == dataset_size
+    return np.array(shard_sizes)
+
+
+def main(args: Namespace) -> None:
+    """Benchmark streaming dataset shuffling methods.
+
+    Args:
+        args (Namespace): Command-line arguments.
+    """
+    names = 'slow', 'med'
+    get_shuffles = get_shuffle_slow, get_shuffle_med
+
+    def wrap(func: Callable):
+        return Caller(func, args.num_canonical_nodes, args.seed, args.epoch, args.timeout)
+
+    callers = list(map(wrap, get_shuffles))
+
+    text = ' '.join(map(lambda s: s.rjust(10), names))
+
+    print(f'{"power".rjust(5)} {"samples".rjust(14)} ' + text)
+    for mul_power in range(args.min_power * args.power_interval,
+                           args.max_power * args.power_interval + 1):
+        power = mul_power / args.power_interval
+        dataset_size = int(2**power)
+        shard_sizes = get_shard_sizes(dataset_size, args.samples_per_shard)
+
+        texts = []
+        for caller in callers:
+            elapsed = caller(shard_sizes)
+            text = f'{elapsed:10.6f}'
+            texts.append(text)
+        text = ' '.join(texts)
+
+        print(f'{power:5.2f} {dataset_size:14,} {text}')
+
+
+if __name__ == '__main__':
+    main(parse_args())
--- streaming/base/shuffle.py
+++ streaming/base/shuffle.py
@@ -3,7 +3,7 @@
 
 """Slice, shuffle, and dice epochs of samples across worker partitions."""
 
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 from numpy.typing import NDArray
@@ -101,8 +101,8 @@ def _partition(shards: List[_Shard], num_parts: int) -> List[List[_Shard]]:
     return lists
 
 
-def get_shuffle(shard_sizes: NDArray[np.int64], num_canonical_nodes: int, seed: int,
-                epoch: int) -> NDArray[np.int64]:
+def get_shuffle_slow(shard_sizes: NDArray[np.int64], num_canonical_nodes: int, seed: int,
+                     epoch: int) -> NDArray[np.int64]:
     """Get the shuffled global ordering of samples for an epoch.
 
     The assignment of shards to nodes is fixed across epochs, but each grouping of shards is
@@ -138,3 +138,108 @@ def get_shuffle(shard_sizes: NDArray[np.int64], num_canonical_nodes: int, seed: 
     # Flatten the shard spans to their sample IDs, then concatenate those into a global list.
     arrs = list(map(_shards_to_samples, parts))
     return np.concatenate(arrs)
+
+
+def divide_spans(spans: List[Tuple[int, int]], num_samples: int, num_parts: int) -> \
+        Tuple[List[Tuple[int, int]], List[Tuple[int, int]]]:
+    """Divide the spans into discrete, equal sized partitions.
+
+    Warning: don't use ``spans`` after this, as its memory is recycled into the returned partitions
+    for performance reasons.
+
+    Args:
+        spans (List[Tuple[int, int]]): List of spans to partition.
+        num_samples (int): Total number of samples across all spans.
+        num_parts (int): Number of groupings to divide spans into.
+
+    Returns:
+        Tuple[List[Tuple, int, int]], List[Tuple[int, int]]]: Spans and super spans.
+    """
+    begin_part = 0
+    span_index = 0
+    samples_so_far = 0
+
+    out_spans = []
+    super_spans = []
+
+    for part in range(num_parts):
+        part_end = num_samples * (part + 1) // num_parts
+
+        while True:
+            if span_index == len(spans):
+                break
+
+            span = spans[span_index]
+            samples_this_span = span[1] - span[0]
+            if part_end < samples_so_far + samples_this_span:
+                if samples_so_far < part_end:
+                    split = part_end - samples_so_far
+                    new_span = span[0], span[0] + split
+                    out_spans.append(new_span)
+                    spans[span_index] = span[0] + split, span[1]
+                    samples_so_far += split
+                break
+
+            out_spans.append(span)
+            span_index += 1
+            samples_so_far += samples_this_span
+
+        super_span = begin_part, len(out_spans)
+        super_spans.append(super_span)
+        begin_part = len(out_spans)
+
+    return spans, super_spans
+
+
+def get_shuffle_med(shard_sizes: NDArray[np.int64], num_canonical_nodes: int, seed: int,
+                    epoch: int) -> NDArray[np.int64]:
+    """Get the shuffled global ordering of samples for an epoch.
+
+    The assignment of shards to nodes is fixed across epochs, but each grouping of shards is
+    processed concurrently in a different order by each node's workers each epoch.
+
+    Args:
+        shard_sizes (NDArray[np.int64]): Number of samples contained in each shard, in order.
+        num_canonical_nodes (int): Number of canonical nodes.
+        seed (int): Base random seed, which is held constant over an entire training run.
+        epoch (int): Current epoch, which is added to the seed to get a different deterministic
+            shuffle each epoch.
+
+    Returns:
+        NDArray[np.int64]: 1:1 mapping of sample ID to shuffled sample ID.
+    """
+    # Create each shards's sample ID span (begin, end excl).
+    spans = []
+    num_samples = 0
+    for shard_size in shard_sizes:
+        span = num_samples, num_samples + shard_size
+        spans.append(span)
+        num_samples += shard_size
+
+    # Generate the initial ordering of shards, which is fixed over an entire training run.
+    run_rng = np.random.default_rng(seed)
+    run_rng.shuffle(spans)
+
+    # Break the shard spans at canonical node boundaries.
+    spans, super_spans = divide_spans(spans, num_samples, num_canonical_nodes)
+
+    # Shuffle the span ordering within each canonical node uniquely to this epoch.
+    epoch_rng = np.random.default_rng(seed + epoch)
+    for begin, end in super_spans:
+        part = spans[begin:end]
+        epoch_rng.shuffle(part)  # pyright: ignore
+        spans[begin:end] = part
+
+    # Populate the global sample ID mapping, shuffling within each span.
+    ids = np.empty(num_samples, np.int64)
+    offset = 0
+    for begin, end in spans:
+        span_size = end - begin
+        ids[offset:offset + span_size] = np.arange(begin, end)
+        epoch_rng.shuffle(ids[offset:offset + span_size])
+        offset += span_size
+
+    return ids
+
+
+get_shuffle = get_shuffle_med
