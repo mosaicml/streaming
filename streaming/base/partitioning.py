3b31de33eecbfb1e87b9c2e20a4e64e5790b620a
James Knighton <iamknighton@gmail.com>
2023-02-28 13:38:25 -0800

    Refactor partitioning to be much faster.

--- streaming/base/partitioning.py
+++ streaming/base/partitioning.py
@@ -1,7 +1,7 @@
 # Copyright 2023 MosaicML Streaming authors
 # SPDX-License-Identifier: Apache-2.0
 
-"""Partitions the sample space to nodes, devices, and workers."""
+"""Partitions the sample space to nodes, ranks, and workers."""
 
 import math
 from typing import Optional
@@ -16,7 +16,7 @@ def get_partitions(num_samples: int,
                    workers_per_rank: int,
                    batch_size: Optional[int] = None,
                    drop_first: int = 0):
-    """Partition the given number of samples to nodes, devices, and workers.
+    """Partition the given number of samples to nodes, ranks, and workers.
 
     Args:
         num_samples (int): Dataset size.
@@ -29,71 +29,76 @@ def get_partitions(num_samples: int,
         drop_first (int): Number of samples seen already, which are dropped. Defaults to ``0``.
 
     Returns:
-        NDArray[np.int64]: Partitions of shape (physical nodes x ranks x workers x samples).
+        NDArray[np.int64]: Partitions (physical nodes x ranks x workers x batches x samples).
     """
-    # Divide the full dataset sample range into sample range per canonical node.
     batch_size = batch_size or 1
-    xx = []
-    for i in range(num_canonical_nodes):
-        a = i * num_samples // num_canonical_nodes
-        z = (i + 1) * num_samples // num_canonical_nodes
-        x = np.arange(a, z)
-        xx.append(x)
 
-    # Make the spans equal length by repeating the last sample if too short.
-    # -> Shape: (canonical nodes x samples).
-    max_len = max(map(len, xx))
-    for i, x in enumerate(xx):
-        if len(x) < max_len:
-            last = np.array([x[-1]])
-            xx[i] = np.concatenate([x, last])
-    x = np.stack(xx)
-
-    # If there are more physical than canonical nodes, interleave canonical onto physical nodes.
-    # -> Shape: (canonical nodes x samples).
+    # Divide the full dataset sample range into a sample range per canonical node.
+    samples_per_canonical_node = (num_samples + num_canonical_nodes - 1) // num_canonical_nodes
+    node_ratio = 0
+    padding = 0
     if num_canonical_nodes < num_physical_nodes:
         assert not num_physical_nodes % num_canonical_nodes
-        ratio = num_physical_nodes // num_canonical_nodes
-        too_many = x.shape[1] % ratio
-        if too_many:
-            too_few = ratio - too_many
-            last = x[:, -ratio - too_few + 1:-ratio + 1]
-            x = np.concatenate([x, last], 1)
+        node_ratio = num_physical_nodes // num_canonical_nodes
+        overflow = samples_per_canonical_node % node_ratio
+        if overflow:
+            padding = node_ratio - overflow
+    padded_samples_per_canonical_node = samples_per_canonical_node + padding
 
-    # Drop samples that have already been seen and reshape.
-    # -> Shape: (physical nodes x samples).
+    # Create the initial sample ID matrix.
+    #
+    # Shape: (canonical nodes, padded samples per canonical node).
+    x = np.arange(num_canonical_nodes * padded_samples_per_canonical_node, dtype=np.int64)
+    x = x.reshape(num_canonical_nodes, padded_samples_per_canonical_node)
+
+    # Make adustments to replicate the original padding and extending behavior.
+    offsets = np.arange(num_canonical_nodes) * padding
+    offsets = np.expand_dims(offsets, 1)
+    x -= offsets
+    starts = np.arange(num_canonical_nodes) * num_samples // num_canonical_nodes
+    starts = np.expand_dims(starts, 1)
+    x += starts - x[:, :1]
+    stops = np.arange(1, 1 + num_canonical_nodes) * num_samples // num_canonical_nodes
+    stops = np.expand_dims(stops, 1)
+    is_shorts = stops - starts < samples_per_canonical_node
+    x[:, samples_per_canonical_node - 1:samples_per_canonical_node] -= is_shorts
+    if padding:
+        x[:, -padding:] = x[:, -padding - node_ratio + 1 - padding:-padding - node_ratio + 1]
+
+    # Flatten, drop samples that have already been seen, reshape back.
+    #
+    # Shape: (physical nodes, samples per node).
     x = x.transpose()
     x = x.flatten()
     x = x[drop_first:]
     x = x.reshape(-1, num_physical_nodes)
     x = x.transpose()
 
-    # Interleave the node sample ranges over each node's devices, padding by repeating last sample.
-    # -> Shape: (physical nodes x samples x devices).
-    too_many = x.shape[1] % ranks_per_node
-    if too_many:
-        too_few = ranks_per_node - too_many
-        last = x[:, -ranks_per_node - too_few + 1:-ranks_per_node + 1]
+    # Interleave the node sample ranges over each node's ranks, padding by repeating the last
+    # sample.
+    #
+    # Shape: (physical nodes, samples per rank, ranks per node).
+    overflow = x.shape[1] % ranks_per_node
+    if overflow:
+        underflow = ranks_per_node - overflow
+        last = x[:, -ranks_per_node - underflow + 1:-ranks_per_node + 1]
         x = np.concatenate([x, last], 1)
     x = x.reshape(num_physical_nodes, -1, ranks_per_node)
 
-    # Interleave each device's samples across its workers, padding with -1.
-    # -> Shape: (physical nodes x samples x workers x devices).
-    too_many = x.shape[1] % workers_per_rank
-    # Make the number of samples multiple of batch size and workers per rank
+    # Pad with -1 adequately for reshaping across workers.
+    #
+    # Shape: (physical nodes, samples per rank, ranks per node).
+    overflow = x.shape[1] % workers_per_rank
     rounded_num_samples = math.ceil(
         x.shape[1] / (workers_per_rank * batch_size)) * (workers_per_rank * batch_size)
-    too_many = rounded_num_samples - x.shape[1]
-    if too_many:
-        last = np.full((num_physical_nodes, too_many, ranks_per_node), -1, np.int64)
+    overflow = rounded_num_samples - x.shape[1]
+    if overflow:
+        last = np.full((num_physical_nodes, overflow, ranks_per_node), -1, np.int64)
         x = np.concatenate([x, last], 1)
 
-    # -> Shape: (physical nodes x ranks per node x samples)
+    # Interleave each rank's padded samples across its workers.
+    #
+    # Shape: (physical nodes, ranks per node, workers per rank, batches per worker, batch size).
     x = x.transpose(0, 2, 1)
-    x = x.reshape(num_physical_nodes, ranks_per_node, -1, batch_size)
-    x = np.concatenate(
-        [x[:, :, np.arange(i, x.shape[2], workers_per_rank), :] for i in range(workers_per_rank)],
-        axis=2)
-
-    # -> Shape: (physical nodes x ranks per node x workers per rank x samples)
-    return x.reshape(num_physical_nodes, ranks_per_node, workers_per_rank, -1)
+    x = x.reshape(num_physical_nodes, ranks_per_node, -1, workers_per_rank, batch_size)
+    return x.transpose(0, 1, 3, 2, 4)
