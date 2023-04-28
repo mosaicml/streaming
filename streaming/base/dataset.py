4ac2cf2409e81c981b49ed2beb7a4cea461adf67
James Knighton <iamknighton@gmail.com>
2023-04-28 00:34:43 -0700

    Sleepy __del__.

--- streaming/base/dataset.py
+++ streaming/base/dataset.py
@@ -407,6 +407,7 @@ class StreamingDataset(Array, IterableDataset):
                 self._locals_shm.buf[:4] = np.int32(0).tobytes()
             except:
                 pass
+        sleep(TICK)
 
     @property
     def size(self) -> int:
