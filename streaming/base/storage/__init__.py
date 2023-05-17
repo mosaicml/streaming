a57dadebe0047e0e6a1d4017550d37c8153459f7
Abhinav Venigalla <abhi@mosaicml.com>
2023-05-17 01:03:38 +0000

    wip

--- streaming/base/storage/__init__.py
+++ streaming/base/storage/__init__.py
@@ -5,9 +5,9 @@
 
 from streaming.base.storage.download import download_file, download_or_wait
 from streaming.base.storage.upload import (AzureUploader, CloudUploader, GCSUploader,
-                                           LocalUploader, OCIUploader, R2Uploader, S3Uploader)
+                                           LocalUploader, OCIUploader, S3Uploader)
 
 __all__ = [
     'download_file', 'download_or_wait', 'CloudUploader', 'S3Uploader', 'GCSUploader',
-    'OCIUploader', 'LocalUploader', 'R2Uploader', 'AzureUploader'
+    'OCIUploader', 'LocalUploader', 'AzureUploader'
 ]
