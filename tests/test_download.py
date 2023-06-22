467f92a882276b94494fe3f9a6a136e4ae462bdc
shivshandilya <127381292+shivshandilya@users.noreply.github.com>
2023-06-22 19:15:43 +0530

    Added files to support azure datalake storage (#311)
    * added files to support azure datalake storage
    
    * updated required libraries
    
    * fixed lint issues
    
    * fixed isort issues with linting
    
    * fixed pre-commit issues
    
    * fix eol issues in docs/source/examples
    
    * updated azure data lake shorthand

--- tests/test_download.py
+++ tests/test_download.py
@@ -10,7 +10,8 @@ import boto3
 import pytest
 from botocore.exceptions import ClientError
 
-from streaming.base.storage.download import (download_file, download_from_azure, download_from_gcs,
+from streaming.base.storage.download import (download_file, download_from_azure,
+                                             download_from_azure_datalake, download_from_gcs,
                                              download_from_local, download_from_s3,
                                              download_or_wait)
 from tests.conftest import GCS_URL, MY_BUCKET, R2_URL
@@ -42,6 +43,7 @@ class TestAzureClient:
             mock_remote_filepath, mock_local_filepath = remote_local_file(
                 cloud_prefix='aaazure://')
             download_from_azure(mock_remote_filepath, mock_local_filepath)
+            download_from_azure_datalake(mock_remote_filepath, mock_local_filepath)
 
 
 class TestS3Client:
@@ -145,6 +147,15 @@ class TestDownload:
     @pytest.mark.usefixtures('remote_local_file')
     def test_download_from_azure_gets_called(self, mocked_requests: Mock, remote_local_file: Any):
         mock_remote_filepath, mock_local_filepath = remote_local_file(cloud_prefix='azure://')
+        download_file(mock_remote_filepath, mock_local_filepath, 60)
+        mocked_requests.assert_called_once()
+        mocked_requests.assert_called_once_with(mock_remote_filepath, mock_local_filepath)
+
+    @patch('streaming.base.storage.download.download_from_azure_datalake')
+    @pytest.mark.usefixtures('remote_local_file')
+    def test_download_from_azure_datalake_gets_called(self, mocked_requests: Mock,
+                                                      remote_local_file: Any):
+        mock_remote_filepath, mock_local_filepath = remote_local_file(cloud_prefix='azure-dl://')
         download_file(mock_remote_filepath, mock_local_filepath, 60)
         mocked_requests.assert_called_once()
         mocked_requests.assert_called_once_with(mock_remote_filepath, mock_local_filepath)
--- tests/test_upload.py
+++ tests/test_upload.py
@@ -9,8 +9,8 @@ from unittest.mock import Mock, patch
 
 import pytest
 
-from streaming.base.storage.upload import (AzureUploader, CloudUploader, GCSUploader,
-                                           LocalUploader, S3Uploader)
+from streaming.base.storage.upload import (AzureDataLakeUploader, AzureUploader, CloudUploader,
+                                           GCSUploader, LocalUploader, S3Uploader)
 from tests.conftest import R2_URL
 
 
@@ -222,6 +222,39 @@ class TestAzureUploader:
             with open(local_file_path, 'w') as _:
                 pass
             _ = AzureUploader(out=local)
+
+
+class TestAzureDataLakeUploader:
+
+    @patch('streaming.base.storage.upload.AzureDataLakeUploader.check_container_exists')
+    @pytest.mark.usefixtures('azure_credentials')
+    @pytest.mark.parametrize('out',
+                             ['azure://container/dir', ('./dir1', 'azure://container/dir/')])
+    def test_instantiation(self, mocked_requests: Mock, out: Any):
+        mocked_requests.side_effect = None
+        _ = AzureDataLakeUploader(out=out)
+        if not isinstance(out, str):
+            shutil.rmtree(out[0])
+
+    @pytest.mark.parametrize('out', ['ss4://container/dir'])
+    def test_invalid_remote_str(self, out: str):
+        with pytest.raises(ValueError, match=f'Invalid Cloud provider prefix.*'):
+            _ = AzureDataLakeUploader(out=out)
+
+    @pytest.mark.parametrize('out', ['ss4://container/dir', ('./dir1', 'gcs://container/dir/')])
+    def test_invalid_remote_list(self, out: Any):
+        with pytest.raises(ValueError, match=f'Invalid Cloud provider prefix.*'):
+            _ = AzureDataLakeUploader(out=out)
+
+    def test_local_directory_is_empty(self, local_remote_dir: Tuple[str, str]):
+        with pytest.raises(FileExistsError, match=f'Directory is not empty.*'):
+            local, _ = local_remote_dir
+            os.makedirs(local, exist_ok=True)
+            local_file_path = os.path.join(local, 'file.txt')
+            # Creating an empty file at specified location
+            with open(local_file_path, 'w') as _:
+                pass
+            _ = AzureDataLakeUploader(out=local)
 
 
 class TestLocalUploader:
