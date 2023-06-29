415707d8aead732824be0abd53194d814096fa84
b-chu <23239305+b-chu@users.noreply.github.com>
2023-06-29 02:29:30 +0000

    Fix tests

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -3,6 +3,7 @@
 
 import hashlib
 import os
+import shutil
 import tempfile
 from typing import Optional
 
@@ -10,32 +11,35 @@ import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
 from streaming import Stream
+from streaming.base.distributed import barrier
 
 
 @pytest.mark.world_size(2)
 def test_local_is_none_with_no_split(monkeypatch: MonkeyPatch) -> None:
-    local = 'local_dir'
     remote = 'remote_dir'
-    monkeypatch.setattr(tempfile, 'gettempdir', lambda: local)
     remote_hash = hashlib.md5(remote.encode('utf-8')).hexdigest()
+    local = os.path.join(tempfile.gettempdir(), remote_hash)
+    shutil.rmtree(local, ignore_errors=True)
+    barrier()
     stream = Stream(remote=remote, local=None)
-    assert os.path.join(local, remote_hash) == stream.local
+    assert local == stream.local
 
 
 @pytest.mark.world_size(2)
 def test_local_is_none_with_split(monkeypatch: MonkeyPatch) -> None:
-    local = 'local_dir'
     remote = 'remote_dir'
-    monkeypatch.setattr(tempfile, 'gettempdir', lambda: local)
     remote_hash = hashlib.md5(remote.encode('utf-8')).hexdigest()
+    local = os.path.join(tempfile.gettempdir(), remote_hash + '_' + 'train')
+    shutil.rmtree(local, ignore_errors=True)
+    barrier()
     stream = Stream(remote=remote, local=None, split='train')
-    assert os.path.join(local, remote_hash + '_' + 'train') == stream.local
+    assert local == stream.local
 
 
 @pytest.mark.world_size(2)
 @pytest.mark.parametrize('split', [None, 'train'])
 def test_local_exists(split: Optional[str]) -> None:
-    local = 'local_dir'
+    local = tempfile.mkdtemp()
     remote = 'remote_dir'
     stream = Stream(remote=remote, local=local, split=split)
     assert local == stream.local
