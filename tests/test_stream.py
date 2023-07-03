9da2e5aef90264d7570653cf502548fc67ca7c04
b-chu <23239305+b-chu@users.noreply.github.com>
2023-07-02 20:04:01 -0700

    Add test for exception and move dist initialization

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -15,7 +15,7 @@ from streaming.base.distributed import barrier
 
 
 @pytest.mark.world_size(2)
-def test_local_is_none_with_no_split(monkeypatch: MonkeyPatch) -> None:
+def test_local_is_none_with_no_split() -> None:
     remote = 'remote_dir'
     remote_hash = hashlib.blake2s(remote.encode('utf-8'), digest_size=16).hexdigest()
     local = os.path.join(tempfile.gettempdir(), remote_hash) + '/'
@@ -23,10 +23,11 @@ def test_local_is_none_with_no_split(monkeypatch: MonkeyPatch) -> None:
     barrier()
     stream = Stream(remote=remote, local=None)
     assert local == stream.local
+    shutil.rmtree(local, ignore_errors=True)
 
 
 @pytest.mark.world_size(2)
-def test_local_is_none_with_split(monkeypatch: MonkeyPatch) -> None:
+def test_local_is_none_with_split() -> None:
     remote = 'remote_dir'
     remote_hash = hashlib.blake2s(remote.encode('utf-8'), digest_size=16).hexdigest()
     local = os.path.join(tempfile.gettempdir(), remote_hash, 'train')
@@ -34,6 +35,7 @@ def test_local_is_none_with_split(monkeypatch: MonkeyPatch) -> None:
     barrier()
     stream = Stream(remote=remote, local=None, split='train')
     assert local == stream.local
+    shutil.rmtree(local, ignore_errors=True)
 
 
 @pytest.mark.world_size(2)
@@ -43,3 +45,16 @@ def test_local_exists(split: Optional[str]) -> None:
     remote = 'remote_dir'
     stream = Stream(remote=remote, local=local, split=split)
     assert local == stream.local
+    shutil.rmtree(local, ignore_errors=True)
+
+
+def test_existing_local_raises_exception(monkeypatch: MonkeyPatch) -> None:
+    local = tempfile.mkdtemp()
+    monkeypatch.setattr(tempfile, 'gettempdir', lambda: local)
+    with pytest.raises(
+            ValueError,
+            match=
+            f'Could not create a local directory. Specify a local directory with the `local` value.'
+    ):
+        _ = Stream()
+    shutil.rmtree(local, ignore_errors=True)
