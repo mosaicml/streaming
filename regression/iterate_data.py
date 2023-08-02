39a3f320bb0b69392224afbe8588c87ae69b81e2
Karan Jariwala <karankjariwala@gmail.com>
2023-08-01 22:48:03 -0700

    Add deterministic shuffling test

--- regression/iterate_data.py
+++ regression/iterate_data.py
@@ -4,28 +4,19 @@
 """Create a streaming dataset from toy data with various options for regression testing."""
 
 import os
-import tempfile
+import time
 import urllib.parse
 from argparse import ArgumentParser, Namespace
+from typing import Union
 
-import utils
+import torch
+from torch import distributed as dist
 from torch.utils.data import DataLoader
+from utils import get_dataloader_params, get_kwargs, get_streaming_dataset_params
 
 from streaming import StreamingDataset
-
-_TRAIN_EPOCHS = 2
-
-
-def get_kwargs(key: str) -> str:
-    """Parse key of named command-line arguments.
-
-    Returns:
-        str: Key of named arguments.
-    """
-    if key.startswith('--'):
-        key = key[2:]
-    key = key.replace('-', '_')
-    return key
+from streaming.base.distributed import (all_gather, barrier, get_rank, get_world_size,
+                                        maybe_init_dist)
 
 
 def parse_args() -> tuple[Namespace, dict[str, str]]:
@@ -35,38 +26,36 @@ def parse_args() -> tuple[Namespace, dict[str, str]]:
         tuple(Namespace, dict[str, str]): Command-line arguments and named arguments.
     """
     args = ArgumentParser()
-    args.add_argument('--cloud_url', type=str)
-    args.add_argument('--check_download', default=False, action='store_true')
-    args.add_argument('--local', default=False, action='store_true')
-    args.add_argument(
-        '--keep_zip',
-        default=False,
-        action='store_true',
-        help=('Whether to keep or delete the compressed form when decompressing'
-              ' downloaded shards for StreamingDataset.'),
-    )
-    args.add_argument(
-        '--shuffle',
-        default=False,
-        action='store_true',
-        help=('Whether to iterate over the samples in randomized order for'
-              ' StreamingDataset.'),
-    )
+    args.add_argument('--epochs',
+                      type=int,
+                      default=2,
+                      help='Number of epochs to iterate over the data')
+    args.add_argument('--validate-files', default=False, action='store_true', help='Verify files')
+    args.add_argument('--shuffling-validation-file',
+                      type=str,
+                      help='File location to store sample order')
+    args.add_argument('--validate-iter-time',
+                      default=False,
+                      action='store_true',
+                      help='Test iter time')
 
     args, runtime_args = args.parse_known_args()
     kwargs = {get_kwargs(k): v for k, v in zip(runtime_args[::2], runtime_args[1::2])}
     return args, kwargs
 
 
-def get_file_count(cloud_url: str) -> int:
+def get_file_count(remote: str) -> Union[int, None]:
     """Get the number of files in a remote directory.
 
     Args:
-        cloud_url (str): Cloud provider url.
+        remote (str): Remote directory URL.
     """
-    obj = urllib.parse.urlparse(cloud_url)
+    obj = urllib.parse.urlparse(remote)
     cloud = obj.scheme
     files = []
+    if cloud == '':
+        return len(
+            [name for name in os.listdir(remote) if os.path.isfile(os.path.join(remote, name))])
     if cloud == 'gs':
         from google.cloud.storage import Bucket, Client
 
@@ -75,12 +64,14 @@ def get_file_count(cloud_url: str) -> int:
 
         bucket = Bucket(gcs_client, obj.netloc)
         files = bucket.list_blobs(prefix=obj.path.lstrip('/'))
+        return sum(1 for f in files if f.key[-1] != '/')
     elif cloud == 's3':
         import boto3
 
         s3 = boto3.resource('s3')
         bucket = s3.Bucket(obj.netloc)
         files = bucket.objects.filter(Prefix=obj.path.lstrip('/'))
+        return sum(1 for f in files if f.key[-1] != '/')
     elif cloud == 'oci':
         import oci
 
@@ -91,8 +82,9 @@ def get_file_count(cloud_url: str) -> int:
         objects = oci_client.list_objects(namespace, obj.netloc, prefix=obj.path.lstrip('/'))
 
         files = objects.data.objects
-
-    return sum(1 for _ in files)
+        return sum(1 for _ in files)
+    else:
+        raise ValueError(f'Unsupported remote directory prefix {cloud} in {remote}')
 
 
 def main(args: Namespace, kwargs: dict[str, str]) -> None:
@@ -102,41 +94,53 @@ def main(args: Namespace, kwargs: dict[str, str]) -> None:
         args (Namespace): Command-line arguments.
         kwargs (dict): Named arguments.
     """
-    tmp_dir = tempfile.gettempdir()
-    tmp_download_dir = os.path.join(tmp_dir, 'test_iterate_data_download')
-    dataset = StreamingDataset(
-        remote=args.cloud_url if args.cloud_url is not None else utils.get_local_remote_dir(),
-        local=tmp_download_dir if args.local else None,
-        split=kwargs.get('split'),
-        download_retry=int(kwargs.get('download_retry', 2)),
-        download_timeout=float(kwargs.get('download_timeout', 60)),
-        validate_hash=kwargs.get('validate_hash'),
-        keep_zip=args.keep_zip,
-        epoch_size=int(kwargs['epoch_size']) if 'epoch_size' in kwargs else None,
-        predownload=int(kwargs['predownload']) if 'predownload' in kwargs else None,
-        cache_limit=kwargs.get('cache_limit'),
-        partition_algo=kwargs.get('partition_algo', 'orig'),
-        num_canonical_nodes=int(kwargs['num_canonical_nodes'])
-        if 'num_canonical_nodes' in kwargs else None,
-        batch_size=int(kwargs['batch_size']) if 'batch_size' in kwargs else None,
-        shuffle=args.shuffle,
-        shuffle_algo=kwargs.get('shuffle_algo', 'py1s'),
-        shuffle_seed=int(kwargs.get('shuffle_seed', 9176)),
-        shuffle_block_size=int(kwargs.get('shuffle_block_size', 1 << 18)),
-    )
+    # Initialize torch dist ourselves, if necessary.
+    destroy_dist = maybe_init_dist()
 
-    dataloader = DataLoader(dataset)
-    for _ in range(_TRAIN_EPOCHS):
-        for _ in dataloader:
-            pass
+    dataset_params = get_streaming_dataset_params(kwargs)
+    dataloader_params = get_dataloader_params(kwargs)
+    dataset = StreamingDataset(**dataset_params)
+    dataloader = DataLoader(dataset=dataset, **dataloader_params)
+    iter_time = []
+    start_time = 0
+    for epoch in range(args.epochs):
+        if epoch > 0:
+            start_time = time.time()
+        for batch in dataloader:
+            if args.shuffling_validation_file is not None:
+                samples = [int(sample) for sample in batch['id']]
+                obj_gather_list = [
+                    torch.zeros(1, dtype=torch.int64).cuda(get_rank())
+                    for _ in range(get_world_size())
+                ]
+                all_gather(obj_gather_list,
+                           torch.Tensor(samples).to(dtype=torch.int64).cuda(get_rank()))
+                barrier()
+                if get_rank() == 0:
+                    with open(args.shuffling_validation_file, 'a') as f:
+                        all_samples = [
+                            str(sample) + '\n'
+                            for tensors in obj_gather_list
+                            for sample in tensors.tolist()
+                        ]
+                        f.writelines(all_samples)
+        if epoch > 0:
+            iter_time.append(time.time() - start_time)
 
-    if args.check_download and args.cloud_url is not None:
-        num_cloud_files = get_file_count(args.cloud_url)
+    if args.validate_files and dataset.streams[0].remote is not None:
+        num_remote_files = get_file_count(dataset.streams[0].remote)
         local_dir = dataset.streams[0].local
         num_local_files = len([
             name for name in os.listdir(local_dir) if os.path.isfile(os.path.join(local_dir, name))
         ])
-        assert num_cloud_files == num_local_files
+        assert num_remote_files == num_local_files, f'Expected {num_remote_files} files, got {num_local_files}'
+
+    # TODO: Assert the iteration time is within a certain threshold
+    if args.validate_iter_time:
+        print(f'Average iter time: {sum(iter_time) / len(iter_time)} secs.')
+
+    if destroy_dist:
+        dist.destroy_process_group()
 
 
 if __name__ == '__main__':
