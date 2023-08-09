3f1fb82ac1aadda5bb9c206653943d43543025c8
Karan Jariwala <karankjariwala@gmail.com>
2023-08-08 17:48:22 -0700

    Added test to validate exception generation when index.json is missing

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -5,13 +5,14 @@ import hashlib
 import os
 import shutil
 import tempfile
-from typing import Optional
+from typing import Any, Optional
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
-from streaming import Stream
+from streaming import Stream, StreamingDataset
 from streaming.base.distributed import barrier
+from tests.common.utils import convert_to_mds
 
 
 @pytest.mark.world_size(2)
@@ -54,3 +55,17 @@ def test_existing_local_raises_exception(monkeypatch: MonkeyPatch) -> None:
     with pytest.raises(ValueError, match=f'Could not create a temporary local directory.*'):
         _ = Stream()
     shutil.rmtree(local, ignore_errors=True)
+
+
+@pytest.mark.usefixtures('local_remote_dir')
+def test_missing_index_json_local(local_remote_dir: Any):
+    num_samples = 117
+    remote_dir, _ = local_remote_dir
+    convert_to_mds(out_root=remote_dir, dataset_name='sequencedataset', num_samples=num_samples)
+    if os.path.exists(os.path.join(remote_dir, 'index.json')):
+        os.remove(os.path.join(remote_dir, 'index.json'))
+    else:
+        raise Exception(f"Missing {os.path.join(remote_dir, 'index.json')}")
+    stream = Stream(remote=None, local=remote_dir)
+    with pytest.raises(RuntimeError, match='No `remote` provided, but local file.*'):
+        _ = StreamingDataset(streams=[stream])
