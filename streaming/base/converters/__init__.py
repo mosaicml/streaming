506d19738bd6c37269f716f3a25668587eaaa10a
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2023-08-21 12:32:36 -0700

    Remove json_to_mds and csv_to_mds methods

--- streaming/base/converters/__init__.py
+++ streaming/base/converters/__init__.py
@@ -3,8 +3,6 @@
 
 """Base module for downloading/uploading files from/to cloud storage."""
 
-from streaming.base.converters.csv_to_mds import csvToMDS
 from streaming.base.converters.dataframe_to_mds import dataframeToMDS
-from streaming.base.converters.json_to_mds import jsonToMDS
 
-__all__ = ['dataframeToMDS', 'csvToMDS', 'jsonToMDS']
+__all__ = ['dataframeToMDS']
