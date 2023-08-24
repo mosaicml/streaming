d8745236e7a37176fa35f17703f4899f55e6866f
Saaketh <narayan.saaketh@gmail.com>
2023-08-24 11:24:13 -0700

    script changes

--- scripts/simulation/simulation_script.py
+++ scripts/simulation/simulation_script.py
@@ -17,7 +17,6 @@ shards = 20850  # number of shards
 samples_per_shard = 4093  # number of samples per shard
 avg_shard_size = 67092639  # average shard size (bytes)
 avg_compressed_shard_size = 16000000  # average compressed shard size (bytes)
-avg_shard_size = 32000000
 
 # training
 epochs = 1
@@ -28,16 +27,16 @@ time_per_sample = 0.018  # time to process one sample on one device (seconds)
 # streaming
 workers = 8  # number of workers per device
 canonical_nodes = 4  # number of canonical nodes
-predownload = 64  # number of samples to predownload per worker (samples)
+predownload = 1  # number of samples to predownload per worker (samples)
 cache_limit = 800000000  # cache limit per node (bytes)
-shuffle_algo = 'py1e'  # shuffling algorithm
+shuffle_algo = 'py1b'  # shuffling algorithm
 shuffle_block_size = 100000  # shuffling block size (samples)
 seed = 17  # random seed
 
 # hardware and network
 physical_nodes = 2  # number of physical nodes
 devices = 8  # number of devices per node
-node_network_bandwidth = 1e9  # network bandwidth per node (bytes/s)
+node_network_bandwidth = 2e9  # network bandwidth per node (bytes/s)
 
 # ---------------------------------------------- #
 
