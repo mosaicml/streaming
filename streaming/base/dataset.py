9f7f57fbc64f9c5fa68a4a3ace1b6da70ce508d5
Karan Jariwala <karankjariwala@gmail.com>
2023-09-08 09:00:43 -0700

    Improved calculation logic

--- streaming/base/dataset.py
+++ streaming/base/dataset.py
@@ -395,7 +395,6 @@ class StreamingDataset(Array, IterableDataset):
         self.shards_per_stream = np.zeros(self.num_streams, np.int64)
         self.sample_offset_per_stream = np.zeros(self.num_streams, np.int64)
         self.samples_per_stream = np.zeros(self.num_streams, np.int64)
-        self.max_shard_size_across_all_streams = 0
         for stream_id, stream in enumerate(self.streams):
             stream_shards = stream.get_shards(world)
             num_stream_samples = sum(map(len, stream_shards))
@@ -403,9 +402,6 @@ class StreamingDataset(Array, IterableDataset):
                 index_filename = os.path.join(stream.local, stream.split, get_index_basename())
                 raise RuntimeError(f'Stream contains no samples: {index_filename}.')
             stream_per_shard += [stream_id] * len(stream_shards)
-            # Includes the size of raw (decompressed) and zip (compressed) shard files.
-            self.max_shard_size_across_all_streams = max(self.max_shard_size_across_all_streams,
-                                                         stream_shards[0].get_max_size())
             self.shard_offset_per_stream[stream_id] = len(self.shards)
             self.shards_per_stream[stream_id] = len(stream_shards)
             self.sample_offset_per_stream[stream_id] = self.num_samples
@@ -423,7 +419,10 @@ class StreamingDataset(Array, IterableDataset):
             if self.cache_limit <= min_cache_usage:
                 raise ValueError(f'Minimum cache usage ({min_cache_usage} bytes) is larger than ' +
                                  f'the cache limit ({self.cache_limit} bytes). Please raise ' +
-                                 f'`cache_limit`.')
+                                 f'`cache_limit`. Recommendation is to provide a `cache_limit` ' +
+                                 f'as high as possible to avoid thrashing.')
+            self.max_shard_size_across_all_streams = max(
+                np.array([shard.get_max_size() for shard in self.shards]))
             if self.cache_limit < 4 * self.max_shard_size_across_all_streams:
                 raise ValueError(f'Cache limit ({self.cache_limit} bytes) is too low. ' +
                                  f'Increase the `cache_limit` to at-least four times the ' +
