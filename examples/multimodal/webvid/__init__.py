026947e39b9a0e96e6ef28421961d0b9d0b72ff4
James Knighton <iamknighton@gmail.com>
2023-11-02 00:16:24 -0700

    streaming/multimodal/ -> examples/multimodal/ (reorganized).

---
+++ examples/multimodal/webvid/__init__.py
@@ -0,0 +1,1 @@
+"""WebVid streaming dataset."""
--- streaming/multimodal/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2023 MosaicML Streaming authors
-# SPDX-License-Identifier: Apache-2.0
-
-"""Natively supported multimodal datasets."""
-
-from streaming.multimodal.webvid import StreamingInsideWebVid as StreamingInsideWebVid
-from streaming.multimodal.webvid import StreamingOutsideDTWebVid as StreamingOutsideDTWebVid
-from streaming.multimodal.webvid import StreamingOutsideGIWebVid as StreamingOutsideGIWebVid
