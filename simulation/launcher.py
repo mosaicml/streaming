a7bcf43090187c0ed330693e25ef13c0d5d4803c
Saaketh <narayan.saaketh@gmail.com>
2023-11-06 13:34:55 -0800

    not fix

--- simulation/launcher.py
+++ simulation/launcher.py
@@ -10,6 +10,6 @@ import subprocess
 def launch_simulation_ui():
     """Launch the simulation UI."""
     absolute_cwd_path = os.path.dirname(os.path.abspath(__file__))
-    sim_file_relative_path = 'interfaces/sim_ui.py'
+    sim_file_relative_path = os.path.join('interfaces', 'sim_ui.py')
     absolute_sim_path = os.path.join(absolute_cwd_path, sim_file_relative_path)
     subprocess.run(['streamlit', 'run', absolute_sim_path])
