baa69165e8432e2369f44acadc196fbdfad08dbc
James Knighton <iamknighton@gmail.com>
2023-12-06 18:10:46 -0800

    usefixtures.

--- tests/test_unsafe_types.py
+++ tests/test_unsafe_types.py
@@ -3,50 +3,49 @@
 
 from shutil import rmtree
 from tempfile import mkdtemp
+from typing import Tuple
 
 import pytest
 
 from streaming import MDSWriter, StreamingDataset
 
 
-def test_do_allow_unsafe_types_safe():
-    local = mkdtemp()
+@pytest.mark.usefixtures('local_remote_dir')
+def test_do_allow_unsafe_types_safe(local_remote_dir: Tuple[str, str]):
+    local, _ = local_remote_dir
     columns = {'num': 'int'}
     with MDSWriter(out=local, columns=columns) as out:
         for num in range(100):
             sample = {'num': num}
             out.write(sample)
     dataset = StreamingDataset(local=local, allow_unsafe_types=True)
-    del dataset
-    rmtree(local)
 
 
-def test_do_allow_unsafe_types_unsafe():
-    local = mkdtemp()
+@pytest.mark.usefixtures('local_remote_dir')
+def test_do_allow_unsafe_types_unsafe(local_remote_dir: Tuple[str, str]):
+    local, _ = local_remote_dir
     columns = {'num': 'pkl'}
     with MDSWriter(out=local, columns=columns) as out:
         for num in range(100):
             sample = {'num': num}
             out.write(sample)
     dataset = StreamingDataset(local=local, allow_unsafe_types=True)
-    del dataset
-    rmtree(local)
 
 
-def test_dont_allow_unsafe_types_safe():
-    local = mkdtemp()
+@pytest.mark.usefixtures('local_remote_dir')
+def test_dont_allow_unsafe_types_safe(local_remote_dir: Tuple[str, str]):
+    local, _ = local_remote_dir
     columns = {'num': 'int'}
     with MDSWriter(out=local, columns=columns) as out:
         for num in range(100):
             sample = {'num': num}
             out.write(sample)
     dataset = StreamingDataset(local=local)
-    del dataset
-    rmtree(local)
 
 
-def test_dont_allow_unsafe_types_unsafe():
-    local = mkdtemp()
+@pytest.mark.usefixtures('local_remote_dir')
+def test_dont_allow_unsafe_types_unsafe(local_remote_dir: Tuple[str, str]):
+    local, _ = local_remote_dir
     columns = {'num': 'pkl'}
     with MDSWriter(out=local, columns=columns) as out:
         for num in range(100):
@@ -54,5 +53,3 @@ def test_dont_allow_unsafe_types_unsafe():
             out.write(sample)
     with pytest.raises(ValueError, match='.*contains an unsafe type.*'):
         dataset = StreamingDataset(local=local)
-        del dataset
-    rmtree(local)
