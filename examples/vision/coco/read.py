18e53c16450de5dc5f692f302af52505f4cd686d
James Knighton <iamknighton@gmail.com>
2023-12-07 13:32:11 -0800

    Dataset kwargs switchover.

--- examples/vision/coco/read.py
+++ examples/vision/coco/read.py
@@ -7,7 +7,7 @@ COCO is a large-scale object detection, segmentation, and captioning dataset. Pl
 `COCO dataset <https://cocodataset.org>`_ for more details.
 """
 
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Dict, Optional
 
 from streaming import StreamingDataset
 
@@ -18,97 +18,13 @@ class StreamingCOCO(StreamingDataset):
     """Implementation of the COCO dataset using StreamingDataset.
 
     Args:
-        remote (str, optional): Remote path or directory to download the dataset from. If ``None``,
-            its data must exist locally. StreamingDataset uses either ``streams`` or
-            ``remote``/``local``. Defaults to ``None``.
-        local (str, optional): Local working directory to download shards to. This is where shards
-            are cached while they are being used. Uses a temp directory if not set.
-            StreamingDataset uses either ``streams`` or ``remote``/``local``. Defaults to ``None``.
-        split (str, optional): Which dataset split to use, if any. If provided, we stream from/to
-            the ``split`` subdirs of  ``remote`` and ``local``. Defaults to ``None``.
-        download_retry (int): Number of download re-attempts before giving up. Defaults to ``2``.
-        download_timeout (float): Number of seconds to wait for a shard to download before raising
-            an exception. Defaults to ``60``.
-        validate_hash (str, optional): Optional hash or checksum algorithm to use to validate
-            shards. Defaults to ``None``.
-        keep_zip (bool): Whether to keep or delete the compressed form when decompressing
-            downloaded shards. If ``False``, keep iff remote is local or no remote. Defaults to
-            ``False``.
-        epoch_size (int, optional): Number of samples to draw per epoch balanced across all
-            streams. If ``None``, takes its value from the total number of underlying samples.
-            Provide this field if you are weighting streams relatively to target a larger or
-            smaller epoch size. Defaults to ``None``.
-        predownload (int, optional): Target number of samples to download per worker in advance
-            of current sample. Workers will attempt to download ahead by this many samples during,
-            but not before, training. Recommendation is to provide a value greater than per device
-            batch size to ensure at-least per device batch size number of samples cached locally.
-            If ``None``, its value gets derived using per device batch size and number of
-            canonical nodes ``max(batch_size, 256 * batch_size // num_canonical_nodes)``.
-            Defaults to ``None``.
-        cache_limit (int, optional): Maximum size in bytes of this StreamingDataset's shard cache.
-            Before downloading a shard, the least recently used resident shard(s) may be evicted
-            (deleted from the local cache) in order to stay under the limit. Set to ``None`` to
-            disable shard eviction. Defaults to ``None``.
-        partition_algo (str): Which partitioning algorithm to use. Defaults to ``orig``.
-        num_canonical_nodes (int, optional): Canonical number of nodes for shuffling with
-            resumption. The sample space is divided evenly according to the number of canonical
-            nodes. The higher the value, the more independent non-overlapping paths the
-            StreamingDataset replicas take through the shards per model replica (increasing data
-            source diversity). Defaults to ``None``, which is interpreted as 64 times the number
-            of nodes of the initial run.
-
-            .. note::
-
-                For sequential sample ordering, set ``shuffle`` to ``False`` and
-                ``num_canonical_nodes`` to the number of physical nodes of the initial run.
-        batch_size (int, optional): Batch size of its DataLoader, which affects how the dataset is
-            partitioned over the workers. Defaults to ``None``.
-        shuffle (bool): Whether to iterate over the samples in randomized order. Defaults to
-            ``False``.
-        shuffle_algo (str): Which shuffling algorithm to use. Defaults to ``py1s``.
-        shuffle_seed (int): Seed for Deterministic data shuffling. Defaults to ``9176``.
-        shuffle_block_size (int): Unit of shuffle. Defaults to ``1 << 18``.
-        transform (callable, optional): A function/transform that takes in an image and bboxes and
-            returns a transformed version. Defaults to ``None``.
+        transform (callable, optional): A function/transform that takes in an image and returns a
+            transformed version. Defaults to ``None``.
+        kwargs (Dict[str, Any]): Keyword arguments.
     """
 
-    def __init__(self,
-                 *,
-                 remote: Optional[str] = None,
-                 local: Optional[str] = None,
-                 split: Optional[str] = None,
-                 download_retry: int = 2,
-                 download_timeout: float = 60,
-                 validate_hash: Optional[str] = None,
-                 keep_zip: bool = False,
-                 epoch_size: Optional[int] = None,
-                 predownload: Optional[int] = None,
-                 partition_algo: str = 'orig',
-                 cache_limit: Optional[int] = None,
-                 num_canonical_nodes: Optional[int] = None,
-                 batch_size: Optional[int] = None,
-                 shuffle: bool = False,
-                 shuffle_algo: str = 'py1s',
-                 shuffle_seed: int = 9176,
-                 shuffle_block_size: int = 1 << 18,
-                 transform: Optional[Callable] = None) -> None:
-        super().__init__(remote=remote,
-                         local=local,
-                         split=split,
-                         download_retry=download_retry,
-                         download_timeout=download_timeout,
-                         validate_hash=validate_hash,
-                         keep_zip=keep_zip,
-                         epoch_size=epoch_size,
-                         predownload=predownload,
-                         cache_limit=cache_limit,
-                         partition_algo=partition_algo,
-                         num_canonical_nodes=num_canonical_nodes,
-                         batch_size=batch_size,
-                         shuffle=shuffle,
-                         shuffle_algo=shuffle_algo,
-                         shuffle_seed=shuffle_seed,
-                         shuffle_block_size=shuffle_block_size)
+    def __init__(self, *, transform: Optional[Callable] = None, **kwargs: Dict[str, Any]) -> None:
+        super().__init__(**kwargs)
         self.transform = transform
 
     def get_item(self, idx: int) -> Any:
