40e9a7a6f89efe605bae3acc9eb1c1082f716e02
James Knighton <iamknighton@gmail.com>
2023-12-11 16:28:31 -0800

    Update streaming/util/importing.py
    Co-authored-by: Karan Jariwala <karankjariwala@gmail.com>

--- streaming/util/importing.py
+++ streaming/util/importing.py
@@ -21,7 +21,7 @@ def get_import_exception_message(package_name: str, extra_deps: str) -> str:
         str: Exception message.
     """
     return f'Streaming was installed without {package_name} support. ' + \
-            f'To use {package_name} related packages with Streaming, run ' + \
+           f'To use {package_name} related packages with Streaming, run ' + \
            f'`pip install \'mosaicml-streaming[{package_name}]\'`.'
 
 
