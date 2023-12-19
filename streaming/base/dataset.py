26c3fc8fa21961394bd9094663e89d97b069054d
snarayan21 <narayan.saaketh@gmail.com>
2023-12-18 20:53:29 -0800

    Logging messages from new defaults only show once per rank. (#543)
    * less logging statements
    
    * logging worker of rank
    
    * logging worker info
    
    * more logging
    
    * using world
    
    * using correct world
    
    * resolved bug
    
    * fixed pytest

--- streaming/base/dataset.py
+++ streaming/base/dataset.py
@@ -394,10 +394,10 @@ class StreamingDataset(Array, IterableDataset):
                           f'This may result in slower batch time. Recommendation is to set ' +
                           f'predownload to at-least batch_size.')
         elif self.predownload is None:
-            warnings.warn(f'Because `predownload` was not specified, it will default to ' +
-                          f'8*batch_size if batch_size is not None, otherwise 64. Prior to ' +
-                          f'Streaming v0.7.0, `predownload` defaulted to ' +
-                          f'max(batch_size, 256 * batch_size // num_canonical_nodes).')
+            logger.warning(f'Because `predownload` was not specified, it will default to ' +
+                           f'8*batch_size if batch_size is not None, otherwise 64. Prior to ' +
+                           f'Streaming v0.7.0, `predownload` defaulted to ' +
+                           f'max(batch_size, 256 * batch_size // num_canonical_nodes).')
             self.predownload = 8 * self.batch_size if self.batch_size is not None else 64
 
         # Convert epoch size from string to int, if needed. Cannot be negative.
@@ -649,13 +649,14 @@ class StreamingDataset(Array, IterableDataset):
         """
         return self.length
 
-    def _set_shuffle_block_size(self):
+    def _set_shuffle_block_size(self, world: World):
         """Set the shuffle block size value."""
         if self.shuffle_block_size is None:
-            warnings.warn(f'Because `shuffle_block_size` was not specified, it will default to ' +
-                          f'max(4_000_000 // num_canonical_nodes, 1 << 18) if ' +
-                          f'num_canonical_nodes is not None, otherwise 262144. Prior to ' +
-                          f'Streaming v0.7.0, `shuffle_block_size` defaulted to 262144.')
+            if not world.worker_of_rank:
+                logger.warning(f'Because `shuffle_block_size` was not specified, it will ' +
+                               f'default to max(4_000_000 // num_canonical_nodes, 1 << 18) if ' +
+                               f'num_canonical_nodes is not None, otherwise 262144. Prior to ' +
+                               f'Streaming v0.7.0, `shuffle_block_size` defaulted to 262144.')
             self.shuffle_block_size = max(4_000_000 // self.num_canonical_nodes, 1 << 18) \
                 if self.num_canonical_nodes is not None else 1 << 18
 
@@ -679,13 +680,16 @@ class StreamingDataset(Array, IterableDataset):
                 if self.shuffle_algo in ['py1s', 'py2s']:
                     self.num_canonical_nodes = 64 * world.num_nodes
                 else:
-                    warnings.warn(f'Because `num_canonical_nodes` was not specified, and ' +
-                                  f'`shuffle_algo` is {self.shuffle_algo}, it will default to ' +
-                                  f'be equal to physical nodes. Prior to Streaming ' +
-                                  f'v0.7.0, `num_canonical_nodes` defaulted to 64 * physical ' +
-                                  f'nodes.')
+                    if not world.worker_of_rank:
+                        print('yo we are here!!!!!')
+                        logger.warning(
+                            f'Because `num_canonical_nodes` was not specified, and ' +
+                            f'`shuffle_algo` is {self.shuffle_algo}, it will default to ' +
+                            f'be equal to physical nodes. Prior to Streaming ' +
+                            f'v0.7.0, `num_canonical_nodes` defaulted to 64 * physical ' +
+                            f'nodes.')
                     self.num_canonical_nodes = world.num_nodes
-            self._set_shuffle_block_size()
+            self._set_shuffle_block_size(world)
             return epoch, 0
 
         # SharedMemory buffers may contain additional null bytes at the end.
@@ -700,13 +704,15 @@ class StreamingDataset(Array, IterableDataset):
                 if self.shuffle_algo in ['py1s', 'py2s']:
                     self.num_canonical_nodes = 64 * world.num_nodes
                 else:
-                    warnings.warn(f'Because `num_canonical_nodes` was not specified, and ' +
-                                  f'`shuffle_algo` is {self.shuffle_algo}, it will default to ' +
-                                  f'be equal to physical nodes. Prior to Streaming ' +
-                                  f'v0.7.0, `num_canonical_nodes` defaulted to 64 * physical ' +
-                                  f'nodes.')
+                    if not world.worker_of_rank:
+                        logger.warning(
+                            f'Because `num_canonical_nodes` was not specified, and ' +
+                            f'`shuffle_algo` is {self.shuffle_algo}, it will default to ' +
+                            f'be equal to physical nodes. Prior to Streaming ' +
+                            f'v0.7.0, `num_canonical_nodes` defaulted to 64 * physical ' +
+                            f'nodes.')
                     self.num_canonical_nodes = world.num_nodes
-            self._set_shuffle_block_size()
+            self._set_shuffle_block_size(world)
             return epoch, 0
 
         # Load the correct resumption meta data.
@@ -717,7 +723,7 @@ class StreamingDataset(Array, IterableDataset):
         # Ensure that we are backwards compatible with old checkpoint dataset state, since the
         # 'initial_physical_nodes' key may not be present.
         self.initial_physical_nodes = obj.get('initial_physical_nodes', None)
-        self._set_shuffle_block_size()
+        self._set_shuffle_block_size(world)
 
         return epoch, sample_in_epoch
 
