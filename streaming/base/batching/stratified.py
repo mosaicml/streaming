e0cf456639173e8b4682d72eb48875a6e4974118
James Knighton <iamknighton@gmail.com>
2024-01-11 17:03:01 -0800

    Update copyright: 2023 -> 2022-2024.

--- streaming/base/batching/stratified.py
+++ streaming/base/batching/stratified.py
@@ -1,4 +1,4 @@
-# Copyright 2023 MosaicML Streaming authors
+# Copyright 2022-2024 MosaicML Streaming authors
 # SPDX-License-Identifier: Apache-2.0
 
 """Apportion shards/samples so each batch has the same amount of samples from each stream."""
--- streaming/base/format/xsv/writer.py
+++ streaming/base/format/xsv/writer.py
@@ -1,4 +1,4 @@
-# Copyright 2023 MosaicML Streaming authors
+# Copyright 2022-2024 MosaicML Streaming authors
 # SPDX-License-Identifier: Apache-2.0
 
 """:class:`XSVWriter` writes samples to `.xsv` files that can be read by :class:`XSVReader`."""
