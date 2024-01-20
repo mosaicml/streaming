487465f67b21cc34a66fcd1ef516120cfb2b98e5
James Knighton <iamknighton@gmail.com>
2024-01-20 03:29:31 -0800

    Borrow some rizz from the future.

---
+++ streaming/base/coord/filesystem/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2022-2024 MosaicML Streaming authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Coordinating using files."""
+
+from streaming.base.coord.filesystem.waiting import (create_file, wait_for_creation,
+                                                     wait_for_deletion)
+
+__all__ = ['create_file', 'wait_for_creation', 'wait_for_deletion']
