aacea8bca902d40f0ceee8067b33a72f6cc66343
James Knighton <iamknighton@gmail.com>
2024-01-21 21:31:26 -0800

    Use `tempfile.gettempdir()` instead of a hardcoded temp root. (#570)
    * Use `tempfile.gettempdir()` instead of a hardcoded temp root.
    
    * Lint.

--- streaming/base/dataset.py
+++ streaming/base/dataset.py
@@ -12,6 +12,7 @@ from concurrent.futures import ThreadPoolExecutor, wait
 from concurrent.futures._base import Future
 from enum import IntEnum
 from math import ceil
+from tempfile import gettempdir
 from threading import Event, Lock
 from time import sleep, time_ns
 from typing import Any, Dict, Iterator, Optional, Sequence, Tuple, Union
@@ -512,7 +513,7 @@ class StreamingDataset(Array, IterableDataset):
         ]
         self._shm_prefix_int, self._locals_shm = get_shm_prefix(streams_local, streams_remote,
                                                                 world)
-        self._filelock_root = os.path.join(os.path.sep, 'tmp', 'streaming')
+        self._filelock_root = os.path.join(gettempdir(), 'streaming')
         os.makedirs(self._filelock_root, exist_ok=True)
 
         # Create the shared memory-backed barrier, without its lock, which is unpickleable.
