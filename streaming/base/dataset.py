23ddca79907be17d589c27272b2d29b7de9fe599
James Knighton <iamknighton@gmail.com>
2024-01-21 21:07:00 -0800

    Lint.

--- streaming/base/dataset.py
+++ streaming/base/dataset.py
@@ -7,12 +7,12 @@ import json
 import logging
 import os
 import sys
-from tempfile import gettempdir
 import warnings
 from concurrent.futures import ThreadPoolExecutor, wait
 from concurrent.futures._base import Future
 from enum import IntEnum
 from math import ceil
+from tempfile import gettempdir
 from threading import Event, Lock
 from time import sleep, time_ns
 from typing import Any, Dict, Iterator, Optional, Sequence, Tuple, Union
