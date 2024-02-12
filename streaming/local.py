5bc99cfbade8fe7614db099b5d4c421473bd02fa
James Knighton <iamknighton@gmail.com>
2024-02-11 21:23:36 -0800

    Replicate needed Stream args to LocalDataset.

--- streaming/local.py
+++ streaming/local.py
@@ -3,12 +3,13 @@
 
 """A non-streaming pytorch map Dataset."""
 
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Sequence, Union
 
 import numpy as np
 from torch.utils.data import Dataset
 
 from streaming.array import Array
+from streaming.format.base.phaser import Phaser
 from streaming.spanner import Spanner
 from streaming.stream.base import Stream
 
@@ -20,15 +21,53 @@ class LocalDataset(Array, Dataset):
 
     Args:
         local (str): Local dataset directory where shards are cached by split.
-        split (str, optional): Which dataset split to use, if any. Defaults to ``None``.
+        split (str, optional): Which dataset split sub-path to use, if any. Defaults to ``None``.
+        allow_schema_mismatch (bool): If ``True``, continue if schemas mismatch across
+            shards, streams, or the whole dataset. If ``False``, raises if schemas mismatch.
+            Defaults to ``False``.
+        allow_unsafe_types (bool): If ``True``, continue if unsafe type(s) are encountered
+            in shard(s). If ``False``, raises if unsafe type(s) encountered. Defaults to ``False``.
+        allow_unchecked_resumption (bool): If ``True``, upon resume, accept and use shard
+            file phases that we are unable to check the size/hash(es) of. If ``False``, upon
+            resume, drop such files, to regenerate on the fly when needed. Defaults to ``True``.
+        validate_hash (str | Sequence[str], optional): Ranked list of hashing algorithms to
+            apply if expected digest is available. Defaults to ``None``.
+        keep_phases (str | Sequence[str] | Dict[str, Optional[bool]] | Phaser): Which phases
+            to keep and to drop upon conversion, given either by intended use case or literally.
+            Specified as a single use or phase to keep, a sequence of uses or phases to keep, a
+            mapping of uses or phases to whether to keep or drop, or a ``Phaser`` (which performs
+            the same keeping or dropping). Defaults to ``None``.
+        kwargs (Any): Any unsupported (for forward compat) or deprecated args.
     """
 
-    def __init__(self, local: str, split: Optional[str] = None):
-        self.stream = Stream(local=local, split=split)
+    def __init__(
+        self,
+        local: str,
+        split: Optional[str] = None,
+        allow_schema_mismatch: bool = False,
+        allow_unsafe_types: bool = False,
+        allow_unchecked_resumption: bool = True,
+        validate_hash: Union[None, str, Sequence[str]] = None,
+        keep_phases: Union[None, str, Sequence[str], Dict[str, Optional[bool]], Phaser] = None,
+        **kwargs: Any,
+    ) -> None:
+        self.stream = Stream(
+            local=local,
+            split=split,
+            allow_schema_mismatch=allow_schema_mismatch,
+            allow_unsafe_types=allow_unsafe_types,
+            allow_unchecked_resumption=allow_unchecked_resumption,
+            download_retry=0,
+            download_timeout=None,
+            download_max_size=None,
+            validate_hash=validate_hash,
+            keep_phases=keep_phases,
+            **kwargs,
+        )
         self.stream.apply_defaults()
         self.stream.download_index()
         self.shards = self.stream.load_index()
-        shard_sizes = np.array([shard.num_samples for shard in self.stream.shards], np.int64)
+        shard_sizes = np.array([shard.num_samples for shard in self.shards], np.int64)
         self.spanner = Spanner(shard_sizes)
         self.num_samples = sum(shard_sizes)
 
