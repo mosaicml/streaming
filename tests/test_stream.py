1864f86ebcd16932862a5139d499185ae6b6c587
James Knighton <iamknighton@gmail.com>
2024-02-12 10:21:29 -0800

    ValueError -> RuntimeError.

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -90,5 +90,5 @@ def test_missing_index_json_local(local_remote_dir: Any):
     else:
         raise Exception(f"Missing {os.path.join(remote_dir, 'index.json')}")
     stream = Stream(remote=None, local=remote_dir)
-    with pytest.raises(ValueError):
+    with pytest.raises(RuntimeError):
         _ = StreamingDataset(streams=[stream])
