1868ef8c22e89e02487c9a3f04bde26bbbf639af
Saaketh <narayan.saaketh@gmail.com>
2024-04-24 10:34:34 -0700

    one client to rule them all

--- streaming/base/storage/sample_download.py
+++ streaming/base/storage/sample_download.py
@@ -5,21 +5,22 @@ import os
 local = "myfile.json"
 remote = "oci://mosaicml-internal-dataset-c4/preconcat-gpt_neox/0pt8/train/index.json"
 
+OCI_CONFIG = oci.config.from_file()
+#no_retry = oci.retry.NoneRetryStrategy()
+OCI_CLIENT = oci.object_storage.ObjectStorageClient(
+    config=OCI_CONFIG, retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY)
+OCI_NAMESPACE = OCI_CLIENT.get_namespace().data
+
 try:
-    config = oci.config.from_file()
-    no_retry = oci.retry.NoneRetryStrategy()
-    client = oci.object_storage.ObjectStorageClient(
-        config=config, retry_strategy=no_retry)
-    namespace = client.get_namespace().data
     obj = urllib.parse.urlparse(remote)
     if obj.scheme != 'oci':
         raise ValueError(
             f'Expected obj.scheme to be `oci`, instead, got {obj.scheme} for remote={remote}')
 
-    bucket_name = obj.netloc.split('@' + namespace)[0]
+    bucket_name = obj.netloc.split('@' + OCI_NAMESPACE)[0]
     # Remove leading and trailing forward slash from string
     object_path = obj.path.strip('/')
-    object_details = client.get_object(namespace, bucket_name, object_path)
+    object_details = OCI_CLIENT.get_object(OCI_NAMESPACE, bucket_name, object_path)
     local_tmp = local + '.tmp'
     with open(local_tmp, 'wb') as f:
         for chunk in object_details.data.raw.stream(2048**2, decode_content=False):
@@ -28,5 +29,4 @@ try:
 except Exception as e:
     raise e
 finally:
-    print(type(object_details.data))
     object_details.data.close()
