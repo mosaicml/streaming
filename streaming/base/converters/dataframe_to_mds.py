12cc19a305f90264b5e0090799d4f98fefdcc6f3
Sean Owen <srowen@gmail.com>
2024-08-12 12:59:45 -0500

    Revert size_limit change

--- streaming/base/converters/dataframe_to_mds.py
+++ streaming/base/converters/dataframe_to_mds.py
@@ -268,10 +268,6 @@ def dataframe_to_mds(dataframe: DataFrame,
         logger.info("Defaulting to zstd compression")
         mds_kwargs['compression'] = 'zstd'
 
-    if 'size_limit' not in mds_kwargs:
-        logger.info("Defaulting size_limit to 1 << 29")
-        mds_kwargs['size_limit'] = 1 << 29
-
     if udf_iterable is not None:
         if 'columns' not in mds_kwargs:
             raise ValueError(
@@ -301,10 +297,11 @@ def dataframe_to_mds(dataframe: DataFrame,
         # storage such as /dbfs or /Volumes in Databricks for example.
         # It's not wrong in this case, but probably nevertheless desirable to specify a local temp
         # path explicitly, to interpret the FUSE-mounted path as remote
-        logger.warning(f"Path {cu.local} is interpreted as a local path. If this is actually " +
-                       "mounted distributed storage, it will work, but consider also specifying " +
-                       "a local temp path. Pass a (local, remote) tuple in out, as in " +
-                       f"('/local_disk0/my_tmp', {cu.local})")
+        logger.warning(
+            f"Path {cu.local} is interpreted as a local path. If this is actually " +
+            "mounted distributed storage, it will work, but consider also specifying " +
+            "a local temp path. Pass a (local, remote) tuple as 'out', as in " +
+            f"('/local_disk0/my_tmp', {cu.local})")
         mds_path = (cu.local, '')
     else:
         mds_path = (cu.local, cu.remote)
