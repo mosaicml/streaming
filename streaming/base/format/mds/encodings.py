6542297802db1963c32ae002d1890695fd4f335e
xiaohanzhangcmu <xiaohanzhang.cmu@gmail.com>
2025-02-27 11:53:30 -0800

    Add Jpeg Array to mds encoding list

--- streaming/base/format/mds/encodings.py
+++ streaming/base/format/mds/encodings.py
@@ -8,13 +8,14 @@ import pickle
 from abc import ABC, abstractmethod
 from decimal import Decimal
 from io import BytesIO
-from typing import Any, Optional
+from typing import Any, Optional, Sequence, List
 
 import numpy as np
 from numpy import typing as npt
 from PIL import Image
 from PIL.JpegImagePlugin import JpegImageFile
 from typing_extensions import Self
+from itertools import chain
 
 __all__ = [
     'get_mds_encoded_size', 'get_mds_encodings', 'is_mds_encoding', 'mds_decode', 'mds_encode',
@@ -531,6 +532,57 @@ class JSON(Encoding):
             e.msg = f'Invalid JSON data: {original}'
             raise
 
+class JPEGArray(Encoding):
+    """encode a list of images as a byte sequence.
+
+    Format:
+        number of images: as the top 4 bytes as a np.uint32
+        sizes of image bytes: following 4 * n_images bytes as np.uint32
+        images: as JPEG bytes concatenated
+    """
+
+    def encode(self, obj: Sequence[bytearray]) -> bytes:
+        if not isinstance(obj, Sequence):
+            raise TypeError(f"expected sequence, got {type(obj)}")
+
+        for byts in obj:
+            if not isinstance(byts, bytearray):
+                raise TypeError(f"expected bytearray, got {type(byts)}")
+
+        image_byte_sizes: List[int] = [len(curr) for curr in obj]
+        n_images_as_bytes: bytes = np.uint32(len(obj)).tobytes()
+        image_byte_sizes_array: npt.NDArray = np.uint32(image_byte_sizes)
+        image_byte_sizes_as_bytes: bytes = image_byte_sizes_array.tobytes()
+        bytes_iterables: Iterator[bytes] = chain([n_images_as_bytes], [image_byte_sizes_as_bytes], obj)
+        return b"".join(bytes_iterables)
+
+    def decode(self, data: bytes) -> List[Image.Image]:
+        # top 4 bytes are np.uint32 size of how many images there are
+        if len(data) < 4:
+            raise ValueError("Input data is too short to contain valid jpeg arrays")
+
+        n_images: int = np.frombuffer(data[:4], dtype=np.uint32)[0]
+
+        if n_images <= 0:
+            raise ValueError("Negative number of images decoded")
+
+        image_bytes_offset = 4 + 4 * n_images
+
+        if len(data) < image_bytes_offset:
+            raise ValueError("Data is too short w.r.t the number of images decoded")
+
+        n_bytes_per_image: Sequence[int] = np.frombuffer(data[4:image_bytes_offset], dtype=np.uint32).tolist()
+        offsets = [image_bytes_offset]
+        for n_bytes in n_bytes_per_image:
+            offsets.append(offsets[-1] + n_bytes)
+
+        result: List[Image.Image] = []
+        for lo, hi in zip(offsets, offsets[1:]):
+            bytes_for_img: bytes = data[lo:hi]
+            decoded = JPEG().decode(bytes_for_img)
+            result.append(decoded)
+        return result
+
 
 # Encodings (name -> class).
 _encodings = {
@@ -554,6 +606,7 @@ _encodings = {
     'str_decimal': StrDecimal,
     'pil': PIL,
     'jpeg': JPEG,
+    'jpeg_array': JPEGArray,
     'png': PNG,
     'pkl': Pickle,
     'json': JSON,
